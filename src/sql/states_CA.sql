228cac0daa73f428a9361110c91adb375be15d05
max <max@cdev.ru>
2009-12-16 07:43:32 +0000

    [*] SQL dump is changed
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@1383 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/sql/states_CA.sql
+++ src/sql/states_CA.sql
@@ -7,7 +7,7 @@ INSERT INTO xlite_states VALUES (193,'Newfoundland and Labrador','NF',0,'CA');
 INSERT INTO xlite_states VALUES (194,'Nova Scotia','NS',0,'CA');
 INSERT INTO xlite_states VALUES (201,'Northwest Territories','NT',0,'CA');
 INSERT INTO xlite_states VALUES (195,'Nunavut','NU',0,'CA');
---UPDATE xlite_states SET code='NU', state='Nunavut' WHERE state='NWT/Nunavut' AND country_code='CA';
+-- UPDATE xlite_states SET code='NU', state='Nunavut' WHERE state='NWT/Nunavut' AND country_code='CA';
 INSERT INTO xlite_states VALUES (196,'Ontario','ON',0,'CA');
 INSERT INTO xlite_states VALUES (197,'Prince Edward Island','PE',0,'CA');
 INSERT INTO xlite_states VALUES (198,'Quebec','QC',0,'CA');
