cd295fc248e9b6eb7a1612c595141f0f3c958acd
max <max@cdev.ru>
2010-01-31 16:24:31 +0000

    [*] config.local.php for CruiseControl is updated
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@1839 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- .dev/build/config.local.php
+++ .dev/build/config.local.php
@@ -1,54 +1,101 @@
 ; <?php /*
-; CruiseControl config file
+; WARNING: Do not change the line above
 ; $Id$ 
+;
+; +-------------------------------------+
+; |   LiteCommerce configuration file   |
+; +-------------------------------------+
+;
+; -----------------
+;  About this file
+; -----------------
+;
 
+;
+; ----------------------
+;  SQL Database details
+; ----------------------
+;
 [database_details]
-hostspec = "localhost:/tmp/mysql-5.0.51.sock"
+hostspec = "localhost"
+socket   = "/tmp/mysql-5.0.51.sock"
+port     = ""
 database = "xcart_xlite_cc"
 username = "xcart"
 password = "fjgQzklfthlj2489g"
 persistent = Off
 
+;
+; -----------------------------------------------------------------------
+;  LiteCommerce HTTP & HTTPS host and web directory where cart installed
+; -----------------------------------------------------------------------
+;
+; NOTE:
+; You should put here hostname ONLY without http:// or https:// prefixes
+; Do not put slashes after the hostname
+; Web dir is the directory in the URL, not the filesystem path
+; Web dir must start with slash and have no slash at the end
+; The only exception is when you configure for the root of the site,
+; in which case you write single slash in it
+;
+; WARNING: Do not set the "$" sign before the parameter names!
+;
+; EXAMPLE 1:
+;
+;   http_host = "www.yourhost.com"
+;   https_host = "www.securedirectories.com/yourhost.com"
+;   web_dir = "/shop"
+;
+; will result in the following URLs:
+;
+;   http://www.yourhost.com/shop
+;   https://www.securedirectories.com/yourhost.com/shop
+;
+;
+; EXAMPLE 2:
+;
+;   http_host = "www.yourhost.com"
+;   https_host = "www.yourhost.com"
+;   web_dir = "/"
+;
+; will result in the following URLs:
+;
+;   http://www.yourhost.com/
+;   https://www.yourhost.com/
+;
 [host_details]
 http_host = "xcart2-530.crtdev.local"
 https_host = "xcart2-530.crtdev.local"
-web_dir = "/~xcart/general/projects/xlite/source/src"
+web_dir = "/~xcart/general/projects/xlite/build/src"
 
+
+;
+; -----------------
+;  Logging details
+; -----------------
+;
 [log_details]
-type = file
+;type = file
+;type = debug
+;type = client
+type = "null"
 name = "var/log/xlite.log.php"
 level = LOG_DEBUG
 ident = "XLite"
 suppress_errors = Off
 suppress_logging_errors = Off
 
-[session_details]
-type = sql
-
+;
+; Skin details
+;
 [skin_details]
 skin = default
 inifile = "layout.ini"
 locale = en
 
-[HTML_Template_Flexy]
-compileDir = "var/run/"
-verbose = Off
-max_forms_per_session = 100
-
-[decorator_details]
-compileDir = "var/run/classes/"
-lockDir = "var/tmp/"
-
-[filesystem_permissions]
-permission_mode = 0
-nonprivileged_permission_dir_all = "0777"
-nonprivileged_permission_file_all = "0666"
-nonprivileged_permission_dir = "0755"
-nonprivileged_permission_file = "0644"
-privileged_permission_dir = "0711"
-privileged_permission_file = "0600"
-privileged_permission_file_nonphp = "0644"
-
+;
+; Profiler settings
+;
 [profiler_details]
 enabled = Off
 eta = On
@@ -56,10 +103,21 @@ eta = On
 [recorder]
 record_queries = off
 
+;
+; ----------------------
+;  PHP settings
+; ----------------------
+;
 [php_settings]
 memory_limit = "32M"
 
+;
+; Installer authcode.
+; A person who do not know the auth code can not access the installation script.
+; Installation authcode is created authomatically and stored in this section.
+;
 [installer_details]
 auth_code = ""
 
+; WARNING: Do not change the line below
 ; */ ?>
