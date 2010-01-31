297a5e6dda4873426e5d60940c4e3c20185c871a
max <max@cdev.ru>
2010-01-31 15:42:05 +0000

    [*] CodeSniffer report script is added
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@1836 194c3f1c-803d-db11-9c9e-000e0c8563f3

---
+++ .dev/phpcs-report.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# $Id$
+#
+# Local PHP Code Shiffer
+#
+# Usage example:
+#
+# ./phpcs-report.sh
+# ./phpcs-report.sh file1
+# ./phpcs-report.sh file1 file2
+#
+
+files_list=""
+
+for f in $@; do
+    [ -r "$f" ] && files_list=$files_list" "$(realpath $f);
+done
+
+cd $(realpath $(dirname $0))
+
+if [ x"${files_list}" = x ]; then
+	files_list=$(realpath ../);
+fi
+
+path=$(realpath ./);
+
+standard=$path/code-sniffs/PEAR
+
+echo Standard: $standard;
+
+$path/phpcs -s --report=full --standard=$standard --ignore=.dev,test/var,test/etc $files_list
