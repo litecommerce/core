105ba82656e1cf762cd5f4b36449aa2ab4b73b6f
svowl <svowl@cdev.ru>
2010-04-08 10:01:26 +0000

    [-] The rest of payment modules removed; CardinalCommerce module removed
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@2516 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- test/sql/xlite_all_modules.sql
+++ test/sql/xlite_all_modules.sql
@@ -2,8 +2,6 @@ UPDATE xlite_modules SET enabled=0;
 UPDATE xlite_modules SET enabled=1 WHERE NOT name IN (
 	'DemoMode',
 	'PayPal',
-	'ProtxDirect',
-	'ProtxForm',
 	'UPS',
 	'ShowcaseOrganizer'
 );
