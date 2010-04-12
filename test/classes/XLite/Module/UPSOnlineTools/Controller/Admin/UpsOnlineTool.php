cc6550a78fba68e71877361367c1d8e219e24694
max <max@cdev.ru>
2010-04-12 12:17:33 +0000

    [*] Small logic changes
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@2559 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- test/classes/XLite/Module/UPSOnlineTools/Controller/Admin/UpsOnlineTool.php
+++ test/classes/XLite/Module/UPSOnlineTools/Controller/Admin/UpsOnlineTool.php
@@ -1,178 +1,190 @@
 <?php
-/*
-+------------------------------------------------------------------------------+
-| LiteCommerce                                                                 |
-| Copyright (c) 2003-2009 Creative Development <info@creativedevelopment.biz>  |
-| All rights reserved.                                                         |
-+------------------------------------------------------------------------------+
-| PLEASE READ  THE FULL TEXT OF SOFTWARE LICENSE AGREEMENT IN THE  "COPYRIGHT" |
-| FILE PROVIDED WITH THIS DISTRIBUTION.  THE AGREEMENT TEXT  IS ALSO AVAILABLE |
-| AT THE FOLLOWING URLs:                                                       |
-|                                                                              |
-| FOR LITECOMMERCE                                                             |
-| http://www.litecommerce.com/software_license_agreement.html                  |
-|                                                                              |
-| FOR LITECOMMERCE ASP EDITION                                                 |
-| http://www.litecommerce.com/software_license_agreement_asp.html              |
-|                                                                              |
-| THIS  AGREEMENT EXPRESSES THE TERMS AND CONDITIONS ON WHICH YOU MAY USE THIS |
-| SOFTWARE PROGRAM AND ASSOCIATED DOCUMENTATION THAT CREATIVE DEVELOPMENT, LLC |
-| REGISTERED IN ULYANOVSK, RUSSIAN FEDERATION (hereinafter referred to as "THE |
-| AUTHOR")  IS  FURNISHING  OR MAKING AVAILABLE TO  YOU  WITH  THIS  AGREEMENT |
-| (COLLECTIVELY,  THE "SOFTWARE"). PLEASE REVIEW THE TERMS AND  CONDITIONS  OF |
-| THIS LICENSE AGREEMENT CAREFULLY BEFORE INSTALLING OR USING THE SOFTWARE. BY |
-| INSTALLING,  COPYING OR OTHERWISE USING THE SOFTWARE, YOU AND  YOUR  COMPANY |
-| (COLLECTIVELY,  "YOU")  ARE ACCEPTING AND AGREEING  TO  THE  TERMS  OF  THIS |
-| LICENSE AGREEMENT. IF YOU ARE NOT WILLING TO BE BOUND BY THIS AGREEMENT,  DO |
-| NOT  INSTALL  OR USE THE SOFTWARE. VARIOUS COPYRIGHTS AND OTHER INTELLECTUAL |
-| PROPERTY  RIGHTS PROTECT THE SOFTWARE. THIS AGREEMENT IS A LICENSE AGREEMENT |
-| THAT  GIVES YOU LIMITED RIGHTS TO USE THE SOFTWARE AND NOT AN AGREEMENT  FOR |
-| SALE  OR  FOR TRANSFER OF TITLE. THE AUTHOR RETAINS ALL RIGHTS NOT EXPRESSLY |
-| GRANTED  BY  THIS AGREEMENT.                                                 |
-|                                                                              |
-| The Initial Developer of the Original Code is Creative Development LLC       |
-| Portions created by Creative Development LLC are Copyright (C) 2003 Creative |
-| Development LLC. All Rights Reserved.                                        |
-+------------------------------------------------------------------------------+
-*/
-
-/* vim: set expandtab tabstop=4 softtabstop=4 foldmethod=marker shiftwidth=4: */
+// vim: set ts=4 sw=4 sts=4 et:
 
 /**
-* Class description.
-*
-* @package Module_UPSOnlineTools
-* @access public
-* @version $Id$
-*
-*/
+ * LiteCommerce
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to licensing@litecommerce.com so we can send you a copy immediately.
+ * 
+ * @category   LiteCommerce
+ * @package    XLite
+ * @subpackage Controller
+ * @author     Creative Development LLC <info@cdev.ru> 
+ * @copyright  Copyright (c) 2010 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version    SVN: $Id$
+ * @link       http://www.litecommerce.com/
+ * @see        ____file_see____
+ * @since      3.0.0
+ */
 
+/**
+ * Register UPS account controller
+ * 
+ * @package XLite
+ * @see     ____class_see____
+ * @since   3.0.0
+ */
 class XLite_Module_UPSOnlineTools_Controller_Admin_UpsOnlineTool extends XLite_Controller_Admin_Abstract
 {
 
-    function getCurrentStep() {
-        $tmp = $this->session->get("ups_step");
-        return intval($tmp);
+    function getCurrentStep()
+	{
+        return intval($this->session->get('ups_step'));
     }
 
-    function getLicense(&$ret) {
-        $obj = new XLite_Module_UPS_Model_Shipping_Ups();
+    function getLicense(&$ret)
+	{
+        $obj = new XLite_Module_UPSOnlineTools_Model_Shipping_Ups();
+
         return $obj->getAgreement($ret);
     }
 
-    function getHaveAccount() {
-        return ($this->config->getComplex('UPSOnlineTools.UPS_username') && $this->config->getComplex('UPSOnlineTools.UPS_password') && $this->config->getComplex('UPSOnlineTools.UPS_accesskey'));
+    function getHaveAccount()
+	{
+        return $this->config->UPSOnlineTools->UPS_username
+			&& $this->config->UPSOnlineTools->UPS_password
+			&& $this->config->UPSOnlineTools->UPS_accesskey;
     }
 
-    function processStep_2() {
-        if ($this->get("confirmed") != 'Y') {
-            $this->set("returnUrl", 'admin.php?target=ups_online_tool&error=license');
-            return false;
+    protected function processStep2()
+	{
+		$result = true;
+
+        if ($this->get('confirmed') != 'Y') {
+            $this->set('returnUrl', $this->buildUrl('ups_online_tool', '', array('error' => 'license')));
+            $result = false;
+
+        } elseif ($this->getLicense($license)) {
+            $this->set('returnUrl', $this->buildUrl('ups_online_tool', '', array('error' => 'http')));
+            $result = false;
         }
-        elseif($this->getLicense($license)) {
-            $this->set("returnUrl", 'admin.php?target=ups_online_tool&error=http');
-            return false;
-        }
-        return true;
+
+        return $result;
     }
 
-    function processStep_3() {
-        $obj = new XLite_Module_UPS_Model_Shipping_Ups();
+    protected function processStep3()
+	{
+        $obj = new XLite_Module_UPSOnlineTools_Model_Shipping_Ups();
         $ret = $this->getReg();
-        if ($tmp= $obj->setAccount($ret, $error)) {
+
+		$result = true;
+        $error = false;
+
+        if ($obj->setAccount($ret, $error)) {
             $this->session->set('ups_message', $error);
-            return false;
+            $result = false;
         }
-        return true;
+
+        return $result;
     }
 
-    function action_next() {
+    function action_next()
+	{
         $cs = $this->getCurrentStep();
-        $func = 'processStep_'.$cs;
-        if(method_exists($this, $func)) 
-            if (!$this->$func()) return;
-        $cs++;
-        $tmp = $this->session->set("ups_step", $cs);
+        $func = 'processStep' . $cs;
+        if (!method_exists($this, $func) || $this->$func()) {
+	        $cs++;
+    	    $this->session->set('ups_step', $cs);
+		}
     }
 
-    function action_cancel() {
-        $tmp = $this->session->set("ups_step", 0);
+    protected function doActionCancel()
+	{
+        $tmp = $this->session->set('ups_step', 0);
     }
 
-    function action_showlicense() {
+    function action_showlicense()
+	{
         $result = $this->getLicense($license);
         echo $license;
 
-		if ($result == 0) {
-			echo <<<EOT
-<p>
-<div align='justify'><font style='FONT-FAMILY: Courier; FONT-SIZE: 10px;'>
-DO YOU AGREE TO ACCESS THE UPS SYSTEMS IN ACCORDANCE WITH AND BE BOUND BY EACHOF THE TERMS AND CONDITIONS SET FORTH ABOVE?<br>
+        if ($result == 0) {
+            echo <<<EOT
+<br />
+<div align="justify"><font style="FONT-FAMILY: Courier; FONT-SIZE: 10px;">
+DO YOU AGREE TO ACCESS THE UPS SYSTEMS IN ACCORDANCE WITH AND BE BOUND BY EACHOF THE TERMS AND CONDITIONS SET FORTH ABOVE?<br />
 
-<input type="radio" name="confirmed" value="Y" OnClick="setConfirmed('Y');"> Yes, I Do Agree
+<input type="radio" id="confirmed_y" name="confirmed" value="Y" onclick="javascript: setConfirmed('Y');"><label for="confirmed_y">Yes, I Do Agree</label>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<input type="radio" name="confirmed" value="N" OnClick="setConfirmed('N');"> No, I Do Not Agree
+<input type="radio" id="confirmed_n" name="confirmed" value="N" onclick="javascript: setConfirmed('N');"><label for="confirmed_n">No, I Do Not Agree</label>
 </div>
 
-<script language="Javascript" type="text/javascript">
-	para = window.parent;
-	if (para) {
-		doc = para.document;
+<script type="text/javascript">
+<!--
+    para = window.parent;
+    if (para) {
+        doc = para.document;
 
-		buttons = doc.getElementById('manage_buttons');
-		if (buttons)
-			buttons.style.display = '';
-	}
+        buttons = doc.getElementById('manage_buttons');
+        if (buttons) {
+            buttons.style.display = '';
+		}
+    }
 
 function setConfirmed(val)
 {
-	para = window.parent;
-	if (para) {
-		doc = para.document;
+    para = window.parent;
+    if (para) {
+        doc = para.document;
 
-		obj = doc.getElementById('confirmedLicense');
-		if (obj)
-			obj.value = val;
-	}
+        obj = doc.getElementById('confirmedLicense');
+        if (obj) {
+            obj.value = val;
+		}
+    }
 }
+-->
 </script>
 EOT;
 
-		}
+        }
 
         exit(0);
     }
 
     function getMessage()
-	{
+    {
         if (!$this->message) {
             $this->message = $this->session->get('ups_message');
             $this->session->set('ups_message', '');
         }
+
         return $this->message; 
     }
 
     function getReg()
-	{
-        $ret = $this->session->get("ups_profile");
-		if (!is_array($ret)) {
-			$ret = array();
-		}
-
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $ret = array_merge($ret, $_POST);
-            $this->session->set("ups_profile", $ret);
+    {
+        $ret = $this->session->get('ups_profile');
+        if (!is_array($ret)) {
+            $ret = array();
         }
 
-        if (empty($ret['software_installer'])) $ret['software_installer'] = 'yes';
+        if (XLite_Core_Request::getInstance()->isPost()) {
+            $ret = array_merge($ret, XLite_Core_Request::getInstance()->getData());
+            $this->session->set('ups_profile', $ret);
+        }
+
+        if (empty($ret['software_installer'])) {
+			$ret['software_installer'] = 'yes';
+		}
+
         return $ret;
     }
 
     function getProfileArray()
-	{
+    {
         $ret = array();
 
-        $profile = $this->auth->getComplex('profile.properties');
+        $profile = $this->auth->getProfile()->get('properties');
         $ret['contact_name'] = $profile['billing_firstname'].' '.$profile['billing_lastname'];
         $ret['title_name'] = $profile['billing_title'];
         $ret['company'] = $profile['billing_company'];
@@ -184,31 +196,29 @@ EOT;
         $ret['phone'] = $profile['billing_phone'];
         $ret['email'] = $profile['login'];
 
-		$ret['state'] = $this->auth->getComplex('profile.billingState.code');
+        $ret['state'] = $this->auth->getProfile()->getComplex('billingState.code');
 
         return $ret;
     }
  
-    function action_fill_from_profile()
-	{
+    protected function doActionFillFromProfile()
+    {
         $profile_arr = $this->getProfileArray();
         $profile_arr = array_merge($this->getReg(), $profile_arr);
-        $this->session->set("ups_profile", $profile_arr);
+
+        $this->session->set('ups_profile', $profile_arr);
     }
 
-	function getUPSStates()
-	{
-		$obj = new XLite_Module_UPS_Model_Shipping_Ups();
-		return $obj->get("upsstates");
-	}
+    function getUPSStates()
+    {
+        $obj = new XLite_Module_UPSOnlineTools_Model_Shipping_Ups();
+        return $obj->getUPSStates();
+    }
 
-	function getUPSCountries()
-	{
-		$obj = new XLite_Module_UPS_Model_Shipping_Ups();
-		return $obj->get("upscountries");
-	}
+    function getUPSCountries()
+    {
+        $obj = new XLite_Module_UPSOnlineTools_Model_Shipping_Ups();
+        return $obj->getUPSCountries();
+    }
 }
-// WARNING :
-// Please ensure that you have no whitespaces / empty lines below this message.
-// Adding a whitespace or an empty line below this line will cause a PHP error.
-?>
+
