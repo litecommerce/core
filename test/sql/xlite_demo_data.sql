aacf2db2e5d8b5b40c986379e6e8d4ead9067133
svowl <svowl@cdev.ru>
2010-05-14 13:34:49 +0000

    [*] LC SQL dump updated: demo shipping rates added; demo user account moved from xlite_demo_data.sql to xlite_demo_user.sql
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3129 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- test/sql/xlite_demo_data.sql
+++ test/sql/xlite_demo_data.sql
@@ -1,7 +1,3 @@
--- Create the default accounts
--- Login / Password - demo@litecommerce.com / user
-INSERT INTO xlite_profiles (profile_id, login, password, access_level, order_id, billing_title, billing_firstname, billing_lastname, billing_phone, billing_address, billing_city, billing_state, billing_country, billing_zipcode, shipping_title, shipping_firstname, shipping_lastname, shipping_phone, shipping_address, shipping_city, shipping_state, shipping_country, shipping_zipcode, first_login, last_login, status, cms_profile_id, cms_name) VALUES (2,'demo@litecommerce.com','ee11cbb19052e40b07aac0ca060c23ee',0,0,'Mr.','Guest','Guest','0123456789','Billing street, 1','Edmond',38,'US','73003','Mr.','Guest','Guest','9876543210','Shipping street, 1','Edmond',38,'US','73003',1053689339,1063630489,'E',2,'____DRUPAL____');
-
 -- Update config table
 UPDATE xlite_config SET comment = 'Check this to temporary close the shop (not available in Demo store)' WHERE name = 'shop_closed';
 UPDATE xlite_config SET value = 'Y' WHERE name = 'customer_security';
