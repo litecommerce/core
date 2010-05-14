1c934e95268af9c2fa8ae24180d50a0216f7fac6
svowl <svowl@cdev.ru>
2010-05-14 07:54:19 +0000

    [*] Release script: config.demo.sh added
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3116 194c3f1c-803d-db11-9c9e-000e0c8563f3

---
+++ .dev/build/release/config.demo.sh
@@ -0,0 +1,38 @@
+# SVN: $Id$
+#
+# Data definition for LiteCommerce release script
+#
+
+. config.sh
+
+# Trigger to enable generation of demo version
+DEMO_VERSION="1"
+
+# LiteCommerce modules for including to the distributives
+XLITE_MODULES=${XLITE_MODULES}"
+Demo
+"
+
+# LiteCommerce files that must be removed from all distributives
+XLITE_FILES_TODELETE="
+install.php
+restoredb
+sql/Makefile
+sql/dbclear.sql
+sql/xlite_all_modules.sql
+sql/xlite_modules_drupal.sql
+sql/xlite_modules_standalone.sql
+"
+
+# Drupal files that must be removed from all distributives
+DRUPAL_FILES_TODELETE="
+profiles/default
+profiles/litecommerce_site
+profiles/litecommerce
+includes/install.pgsql.inc
+"
+
+LC_SEO_PHRASES=""
+
+DRUPAL_SEO_PHRASES=""
+
