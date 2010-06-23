1fbe41f604cfdbd5283777a72bddce622e160633
max <max@cdev.ru>
2010-06-23 13:03:00 +0000

    M:94134 [*] Templates structure debug subsystem is added
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3423 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/skins/drupal/en/js/template_debuger.js
+++ src/skins/drupal/en/js/template_debuger.js
@@ -1,7 +1,7 @@
 /* vim: set ts=2 sw=2 sts=2 et: */
 
 /**
- * ____file_title____
+ * Templates debugger
  *  
  * @author  Creative Development LLC <info@cdev.ru> 
  * @copyright Copyright (c) 2010 Creative Development LLC <info@cdev.ru>. All rights reserved
@@ -11,163 +11,264 @@
  * @since   3.0.0
  */
 
-function getComments(p)
+// Display debug canvas and info box
+function displayTemplateInfo(event)
 {
-  var list = [];
+  if (
+    typeof(event.target.templates) != 'undefined'
+    && event.target.templates.length
+    && window.templateDebugSwitcher.enabled
+    && (!window.templateDebugSwitcher.current || !window.templateDebugSwitcher.current.isSameNode(event.target))
+  ) {
 
-  if (p.childNodes) {
-    for (var i = 0; i < p.childNodes.length; i++) {
-      if (p.childNodes[i].nodeType == 8) {
-        list.push(p.childNodes[i]);
+    $('.tpl-debug-canvas').remove();
+    window.templateDebugSwitcher.infoContent.children().remove();
 
-      } else if (p.childNodes[i].childNodes) {
-        list = list.concat(getComments(p.childNodes[i]));
+    var s = 36;
+
+    for (var i = 0; i < event.target.templates.length; i++) {
+      var t = event.target.templates[i];
+      var c = document.body.appendChild(document.createElement('div'));
+      c.className = i == 0
+        ? 'tpl-debug-canvas tpl-debug-current'
+        : ('tpl-debug-canvas tpl-debug-' + (t.list ? 'list' : 'tpl') + '-canvas');
+
+      c = $(c);
+
+      var beginPos = t.begin.offset();
+      var endPos = t.end.offset();
+
+      c.css(
+        {
+          top:    beginPos.top + 'px',
+          left:   beginPos.left + 'px',  
+          width:  (endPos.left - beginPos.left + t.end.outerWidth()) + 'px',
+          height: (endPos.top - beginPos.top + t.end.outerHeight()) + 'px'
+        }
+      );
+
+      if (i == 0) {
+        window.templateDebugSwitcher.region = {
+          top:    beginPos.top,
+          left:   beginPos.left,
+          right:  (endPos.left + t.end.outerWidth()),
+          bottom: (endPos.top + t.end.outerHeight())
+        };
       }
-    }
-  }
 
-  return list;
+      var li = document.createElement('li');
+      if (i == event.target.templates.length - 1) {
+        li.className = 'last';
+      }
+      li.innerHTML = t.list
+        ? '<strong>List:</strong> ' + t.list
+        : t.tpl + '<br /><strong>Template ID:</strong> ' + t.id + '; <strong>Class:</strong> ' + t.class;
+      window.templateDebugSwitcher.infoContent.append(li);
+    }
+
+    window.templateDebugSwitcher.current = event.target;
+
+    window.templateDebugSwitcher.infoBox.show();
+    checkTemplateRegion(event);
+  }
 }
 
-function createCommentBox(begin, end)
+// Check tempalte region by onmousemove
+function checkTemplateRegion(event)
 {
-  if (!end.parentNode || !begin.parentNode || !begin.parentNode.isSameNode(end.parentNode)) {
-    return;
+  if (window.templateDebugSwitcher.region && window.templateDebugSwitcher.enabled) {
+
+    var r = window.templateDebugSwitcher.region;
+
+    if (r.top < event.pageY && r.bottom > event.pageY && r.left < event.pageX && r.right > event.pageX) {
+
+      window.templateDebugSwitcher.infoBox.css(
+        {
+          top:  (event.pageY + 5) + 'px',
+          left: (event.pageX + 5) + 'px'
+        }
+      );
+
+    } else {
+
+      $('.tpl-debug-canvas').remove();
+      window.templateDebugSwitcher.infoBox.hide();
+      window.templateDebugSwitcher.current = null;
+      window.templateDebugSwitcher.region = null;
+
+    }
   }
+}
 
-  var tn = 'div';
-
-  var firstNode = begin.nextSibling;
-  while (firstNode && !firstNode.tagName) {
-    firstNode = firstNode.nextSibling;
-  }
-
-  if (!firstNode) {
-    return;
-  }
-
-  switch (firstNode.tagName.toLowerCase()) {
-    case 'tr':
-      tn = 'tbody';
+// Find next sibling elements - visible and not script
+function findNextVisibleElement(elm)
+{
+  while (elm.nextSibling) {
+    elm = elm.nextSibling;
+    if (elm.nodeType == 1 && elm.tagName.toUpperCase() != 'SCRIPT' && elm.style.display != 'none') {
       break;
-  }
-
-  var node = begin.nextSibling;
-
-  var list = [];
-  var length = 0;
-
-  while (node && !node.isEqualNode(end)) {
-    node.parentNode.removeChild(node);
-    if (node.nodeType != 8) {
-      length++;
     }
-    list.push(node);
-    node = begin.nextSibling;
   }
 
-  if (!node) {
-    return;
-  }
-
-  var m = begin.data.match(/[ ]+([^ ]+) : ([^ ]+) \(([0-9]+)\) (?:\['([^ ]+)')?/);
-  if (!m) {
-    return;
-  }
-
-  var info = '<strong>Class name:</strong> ' + m[1] + ';'
-    + '<strong>Template relative path:</strong> ' + m[2] + ';'
-    + '<strong>Id:</strong> ' + m[3] + ';';
-
-  if (m[4]) {
-    info += '<strong>List name:</strong> ' + m[4] + ';';
-  }
-
-  if (1 == length) {
-    for (var i = 0; i < list.length; i++) {
-      if (list[i].nodeType != 8) {
-        list[i].className += ' debug-box';
-        list[i]._debugInfo = info;
-        $(list[i]).mouseover(displayTemplateDebugInfo);
-        break;
-      }
-    }
-
-  } else {
-
-    var box = document.createElement(tn);
-    box.className = 'debug-box';
-
-    for (var i = 0; i < list.length; i++) {
-      box.appendChild(list[i]);
-    }
-
-    begin.parentNode.insertBefore(box, end);
-    box._debugInfo = info;
-    $(box).mouseover(displayTemplateDebugInfo);
-  }
+  return elm;
 }
 
-function displayTemplateDebugInfo(event)
+// Find previoues sibling elements - visible and not script
+function findPreviousVisibleElement(elm)
 {
-  event.stopPropagation();
-
-  var p = event.target;
-
-  document.getElementById('debug_template_panel').clear();
-
-  while (p && p.parentNode) {
-    if (p._debugInfo) {
-      document.getElementById('debug_template_panel').add(p._debugInfo);
+  while (elm.previousSibling) {
+    elm = elm.previousSibling;
+    if (elm.nodeType == 1 && elm.tagName.toUpperCase() != 'SCRIPT' && elm.style.display != 'none') {
+      break;
     }
-    p = p.parentNode;
   }
+
+  return elm;
 }
+
 
 $(document).ready(
   function() {
 
-    var debug = document.createElement('div');
-    debug.id = 'debug_template_panel';
-    debug.appendChild(document.createElement('ul'));
-    document.body.appendChild(debug);
+    // Create switcher panel
+    var switcher = document.body.appendChild(document.createElement('div'));
+    switcher.className = 'tpl-debug-switcher';
+    var box = switcher.appendChild(document.createElement('div'));
 
-    debug.clear = function() {
-      if (this.to) {
-        clearTimeout(this.to);
-        this.to = null;
+    // Add link
+    switcher.link = box.appendChild(document.createElement('a'))
+    switcher.link.appendChild(document.createElement('span')).innerHTML = 'Disable';
+    $(switcher.link).click(
+      function(event) {
+        event.stopPropagation();
+        switcher.switchDebugging();
+        return false;
       }
-      this.style.display = 'none';
-      $('li', this).remove();
+    );
+
+    switcher.link.appendChild(document.createTextNode(' tracing'));
+
+    // Add service properties and methods
+    switcher.enabled = true;
+
+    switcher.switchDebugging = function() {
+      this.enabled = !this.enabled;
+      $('span', switcher.link).html(this.enabled ? 'Disable' : 'Enable');
+      if (!this.enabled) {
+        $('.tpl-debug-canvas').remove();
+        this.infoContent.children().remove();
+        this.current = null;
+        this.region = null;
+        $(switcher.link).addClass('disabled');
+
+      } else {
+        $(switcher.link).removeClass('disabled');
+      }
     }
 
-    debug.add = function(html) {
-      var li = document.createElement('li');
-      li.innerHTML = html;
-      li.microTime = this.currentMT;
-      $('ul', this).get(0).appendChild(li);
+    switcher.infoBox = document.body.appendChild(document.createElement('div'));
+    switcher.infoContent = $(switcher.infoBox.appendChild(document.createElement('ul')));
+    switcher.infoBox = $(switcher.infoBox);
+    switcher.infoBox.addClass('tpl-debug-info').hide();
 
-      this.style.display = '';
-    }
+    switcher.current = null;
+    switcher.region = null;
 
-    var beginPattern = new RegExp(' \{\{\{ ');
+    window.templateDebugSwitcher = switcher;
 
-    var list = getComments(document.documentElement);
-    for (var i = 0; i < list.length; i++) {
-      if (-1 != list[i].data.search(beginPattern)) {
-        var begin = list[i];
-        var end = false;
-        var endPattern = new RegExp(' \}\}\}' + begin.data.replace(/\).+$/, ')').replace(/(\(|\)|\/)/g, '.'));
-        for (var n = i + 1; n < list.length && !end; n++) {
-          if (-1 != list[n].data.search(endPattern)) {
-            end = list[n];
+    // Assign global event listener
+    $(document.body).mousemove(checkTemplateRegion);
+
+    // Assign event listeners
+    $('*').filter(
+      function() {
+        return this.nodeType == 1 && this.innerHTML.search(/<[a-z]/) == -1;
+      }
+    )
+      .mouseover(displayTemplateInfo)
+      .each(
+        function() {
+          this.templates = [];
+
+          var p = this;
+          // Collect comments-with-debug-info
+          while (p && p.parentNode) {
+            var elm = p;
+            while (elm.previousSibling) {
+              elm = elm.previousSibling;
+              if (elm.nodeType == 8 && elm.data.search(/ \{\{\{ /) != -1) {
+
+                // Found begin comment tag
+                var begin = elm;
+
+                // Search end comment tag
+                var end = null;
+                var endPattern = new RegExp(' \}\}\}' + begin.data.replace(/\).+$/, ')').replace(/(\(|\)|\/)/g, '.'));
+                var ns = p;
+                while (ns.nextSibling && !end) {
+                  ns = ns.nextSibling;
+                  if (ns.nodeType == 8 && ns.data.search(endPattern) != -1) {
+                    end = ns;
+                  }
+                }
+
+                if (end) {
+
+                  // Found begin and end comment tags; parse begin comment tag
+                  var m = begin.data.match(/[ ]+([^ ]+) : ([^ ]+) \(([0-9]+)\) (?:\['([^ ]+)')?/);
+                  if (m) {
+                    this.templates.push(
+                      {
+                        begin: $(findNextVisibleElement(begin)),
+                        end:   $(findPreviousVisibleElement(end)),
+                        class: m[1],
+                        tpl:   m[2],
+                        id:    m[3],
+                        list:  false
+                      }
+                    );
+
+                    if (m[4]) {
+
+                      // Comment tag is list child - detect list borders
+                      var listFirst = null;
+                      var listEnd = null;
+                      var beginPattern = new RegExp('\'' + m[4] + '\' list child. +\{\{\{');
+                      for (var i = 0; begin.parentNode.childNodes.length && !listFirst; i++) {
+                        var n = begin.parentNode.childNodes[i];
+                        if (n.nodeType == 8 && n.data.search(beginPattern) != -1) {
+                          listFirst = n;
+                        }
+                      }
+
+                      var endPattern = new RegExp('\}\}\} .+\'' + m[4] + '\' list child');
+                      for (var i = begin.parentNode.childNodes.length - 1; i >= 0 && !listEnd; i--) {
+                        var n = begin.parentNode.childNodes[i];
+                        if (n.nodeType == 8 && n.data.search(endPattern) != -1) {
+                          listEnd = n;
+                        } 
+                      }
+
+                      if (listFirst && listEnd) {
+
+                        // List borders found
+                        this.templates.push(
+                          {
+                            begin: $(findNextVisibleElement(listFirst)),
+                            end:   $(findPreviousVisibleElement(listEnd)),
+                            list:  m[4]
+                          }
+                        );
+                      }
+                    }
+                  }
+                }
+              }
+            }
+
+            p = p.parentNode;
           }
         }
-
-        if (end) {
-          createCommentBox(begin, end);
-        }
-      }
-    }
+    );
   }
 );
