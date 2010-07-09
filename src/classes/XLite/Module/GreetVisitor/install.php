cc70e6a3b47a48eeecc3d4c333f0b87a0538b5ca
max <max@cdev.ru>
2010-07-09 16:06:51 +0000

    [*] Small logic changes
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3489 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/classes/XLite/Module/GreetVisitor/install.php
+++ src/classes/XLite/Module/GreetVisitor/install.php
@@ -109,7 +109,9 @@ $MODULE_NAME = "GreetVisitor";
 start_patching($MODULE_NAME);
 
 if (is_object($this)) {
-    $schema = (!empty($this->layout))?$this->layout:($this->config->Skin->skin'));
+    $schema = !empty($this->layout)
+		? $this->layout
+		: $this->config->Skin->skin;
 }
 
 //////////////////////////////////////
