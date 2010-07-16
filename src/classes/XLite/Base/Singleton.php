d5ae95058e4eaeb2bbfed04a3f2fedf5a9d8ec7e
vvs <vvs@cdev.ru>
2010-07-16 22:11:49 +0000

    [+] So called "super class" and the Singleton abstract class have been added [*] The "\Base" was made abstract (check it; it's the possible reason of errors)
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3544 194c3f1c-803d-db11-9c9e-000e0c8563f3

---
+++ src/classes/XLite/Base/Singleton.php
@@ -0,0 +1,70 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * LiteCommerce
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to licensing@litecommerce.com so we can send you a copy immediately.
+ * 
+ * @category   LiteCommerce
+ * @package    XLite
+ * @subpackage Base
+ * @author     Creative Development LLC <info@cdev.ru> 
+ * @copyright  Copyright (c) 2010 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version    SVN: $Id$
+ * @link       http://www.litecommerce.com/
+ * @see        ____file_see____
+ * @since      3.0.0
+ */
+
+namespace XLite\Base;
+
+/**
+ * Singleton 
+ * 
+ * @package    XLite
+ * @see        ____class_see____
+ * @since      3.0.0
+ */
+abstract class Singleton extends SuperClass
+{
+    /**
+     * Array of instances for all derived classes
+     *
+     * @var    array
+     * @access protected
+     * @see    ____var_see____
+     * @since  3.0
+     */
+    protected static $instances = array();
+
+
+    /**
+     * Method to access a singleton
+     *
+     * @return \XLite\Base
+     * @access public
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    public static function getInstance()
+    {
+        $className = get_called_class();
+
+        // Create new instance of the object (if it is not already created)
+        if (!isset(static::$instances[$className])) {
+            static::$instances[$className] = new $className();
+        }
+
+        return static::$instances[$className];
+    }
+}
