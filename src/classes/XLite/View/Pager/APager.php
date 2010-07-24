68c05cada83004059890b9cdea7947d73cfcc1ec
vvs <vvs@cdev.ru>
2010-07-24 18:34:34 +0000

    E:0038239 [+] Some stubs for the new style pager
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3596 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/classes/XLite/View/Pager/APager.php
+++ src/classes/XLite/View/Pager/APager.php
@@ -29,7 +29,7 @@
 namespace XLite\View\Pager;
 
 /**
- * APager 
+ * Abstract pager class
  * 
  * @package    XLite
  * @see        ____class_see____
@@ -37,4 +37,76 @@ namespace XLite\View\Pager;
  */
 abstract class APager extends \XLite\View\AView
 {
+    /**
+     * It's the defeult valueof items to display per page 
+     */
+    const ITEMS_PER_PAGE_DEFAULT = 10;
+
+
+    /**
+     * Return list of items to display on the current page 
+     * 
+     * @param int $start index of the first item on the page
+     * @param int $count number of items per page
+     *  
+     * @return array|\Doctrine\ORM\PersistentCollection
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    abstract protected function getDataFrame($start, $count);
+
+
+    /**
+     * Return current page ID 
+     * 
+     * @return int
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function getPageId()
+    {
+        return intval(\XLite\Core\Request::getInstance()->pageId);
+    }
+
+    /**
+     * Return index of the first item on the current page
+     * 
+     * @return int
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function getStartItem()
+    {
+        return $this->getPageId() * $this->getItemsPerPage();
+    }
+
+    /**
+     * Return number of items per page 
+     * 
+     * @return int
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function getItemsPerPage()
+    {
+        return self::ITEMS_PER_PAGE_DEFAULT;
+    }
+
+
+    /**
+     * Return page data 
+     * 
+     * @return array|\Doctrine\ORM\PersistentCollection
+     * @access public
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    public function getData()
+    {
+        return $this->getDataFrame($this->getStartItem(), $this->getItemsPerPage());
+    }
 }
--- src/classes/XLite/View/Pager/ProductsList/CategoryProducts.php
+++ src/classes/XLite/View/Pager/ProductsList/CategoryProducts.php
@@ -16,7 +16,7 @@
  * 
  * @category   LiteCommerce
  * @package    XLite
- * @subpackage Controller
+ * @subpackage View
  * @author     Creative Development LLC <info@cdev.ru> 
  * @copyright  Copyright (c) 2010 Creative Development LLC <info@cdev.ru>. All rights reserved
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
@@ -26,39 +26,45 @@
  * @since      3.0.0
  */
 
-namespace XLite\Controller\Customer;
+namespace XLite\View\Pager\ProductsList;
 
 /**
- * Category
+ * Pager for the category products page
  * 
- * @package XLite
- * @see     ____class_see____
- * @since   3.0.0
+ * @package    XLite
+ * @see        ____class_see____
+ * @since      3.0.0
  */
-class Category extends Catalog
+class CategoryProduct extends AProductsList
 {
     /**
-     * getModelObject
+     * Return current category model object
      *
-     * @return \XLite\Model\AModel
+     * TODO - check if it's needed to use this widget outside the Catalog controller.
+     * If yes, add the "category" widget  parameter
+     * 
+     * @return \XLite\Model\Category
      * @access protected
+     * @see    ____func_see____
      * @since  3.0.0
      */
-    protected function getModelObject()
+    protected function getCategory()
     {
-        return $this->getCategory();
+        return $this->__call(__FUNCTION__);
     }
 
-
     /**
-     * handleRequest 
-     * 
-     * @return void
-     * @access public
+     * Return list of items to display on the current page
+     *
+     * @param int $start index of the first item on the page
+     * @param int $count number of items per page
+     *
+     * @return array|\Doctrine\ORM\PersistentCollection
+     * @access protected
+     * @see    ____func_see____
      * @since  3.0.0
      */
-    public function handleRequest()
+    protected function getDataFrame($start, $count)
     {
-        is_null($this->getCategory()) ? $this->setReturnUrl($this->buildURL('main')) : parent::handleRequest();
     }
 }
