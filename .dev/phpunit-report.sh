287e0c693c64e3ed9a1173df2990d3885b776467
max <max@cdev.ru>
2010-07-28 05:19:47 +0000

    [*] Logic chanegs in  phpunit-report.sh
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3622 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- .dev/phpunit-report.sh
+++ .dev/phpunit-report.sh
@@ -13,7 +13,7 @@ cd $(dirname $0)
 cd ..
 RP=`realpath .dev/tests/AllTests.php`
 CP=`realpath ./coverage`
-/u/xcart/bin/phpunit-report --coverage-html $CP xliteAllTests $RP $1
+phpunit --coverage-html $CP xliteAllTests .dev/tests/AllTests.php $1
 
 P=`realpath ./ | replace '/u/'$USER'/public_html' ''`
 echo 'Open coverage report http://xcart2.crtdev.local/~'$USER$P'/coverage link';
