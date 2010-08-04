c845a520e32a373c47652f12e8a2d43147eb701b
max <max@cdev.ru>
2010-08-04 10:55:25 +0000

    [!] Bug: File cache class has parent private method. Fixed.
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3669 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/classes/XLite/Core/FileCache.php
+++ src/classes/XLite/Core/FileCache.php
@@ -37,35 +37,35 @@ namespace XLite\Core;
  */
 class FileCache extends \Doctrine\Common\Cache\AbstractCache
 {
-	/**
-	 * Cache directory path 
-	 * 
-	 * @var    string
-	 * @access protected
-	 * @see    ____var_see____
-	 * @since  3.0.0
-	 */
-	protected $path = null;
+    /**
+     * Cache directory path 
+     * 
+     * @var    string
+     * @access protected
+     * @see    ____var_see____
+     * @since  3.0.0
+     */
+    protected $path = null;
 
-	/**
-	 * File header 
-	 * 
-	 * @var    string
-	 * @access protected
-	 * @see    ____var_see____
-	 * @since  3.0.0
-	 */
-	protected $header = '<?php die(); ?>';
+    /**
+     * File header 
+     * 
+     * @var    string
+     * @access protected
+     * @see    ____var_see____
+     * @since  3.0.0
+     */
+    protected $header = '<?php die(); ?>';
 
-	/**
-	 * File header length 
-	 * 
-	 * @var    integer
-	 * @access protected
-	 * @see    ____var_see____
-	 * @since  3.0.0
-	 */
-	protected $headerLength = 15;
+    /**
+     * File header length 
+     * 
+     * @var    integer
+     * @access protected
+     * @see    ____var_see____
+     * @since  3.0.0
+     */
+    protected $headerLength = 15;
 
     /**
      * TTL block length 
@@ -77,54 +77,54 @@ class FileCache extends \Doctrine\Common\Cache\AbstractCache
      */
     protected $ttlLength = 11;
 
-	/**
-	 * Constructor
-	 * 
-	 * @return void
-	 * @access public
-	 * @see    ____func_see____
-	 * @since  3.0.0
-	 */
-	public function __construct($path = null)
-	{
-		$this->setPath($path ? $path : sys_get_temp_dir());
-	}
+    /**
+     * Constructor
+     * 
+     * @return void
+     * @access public
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    public function __construct($path = null)
+    {
+        $this->setPath($path ? $path : sys_get_temp_dir());
+    }
 
-	/**
-	 * Set cache path 
-	 * 
-	 * @param string $path Path
-	 *  
-	 * @return void
-	 * @access public
-	 * @see    ____func_see____
-	 * @since  3.0.0
-	 */
-	public function setPath($path)
-	{
-		if (is_string($path)) {
-			if (!file_exists($path)) {
-				\Includes\Utils\FileManager::mkdirRecursive($path);
-			}
+    /**
+     * Set cache path 
+     * 
+     * @param string $path Path
+     *  
+     * @return void
+     * @access public
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    public function setPath($path)
+    {
+        if (is_string($path)) {
+            if (!file_exists($path)) {
+                \Includes\Utils\FileManager::mkdirRecursive($path);
+            }
 
             if (file_exists($path) && is_dir($path)) {
-				$this->path = $path;
-			}
-		}
-	}
+                $this->path = $path;
+            }
+        }
+    }
 
-	/**
-	 * Get cache path 
-	 * 
-	 * @return string
-	 * @access public
-	 * @see    ____func_see____
-	 * @since  3.0.0
-	 */
-	public function getPath()
-	{
-		return $this->path;
-	}
+    /**
+     * Get cache path 
+     * 
+     * @return string
+     * @access public
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
 
     /**
      * Get cache repository ids list 
@@ -136,22 +136,16 @@ class FileCache extends \Doctrine\Common\Cache\AbstractCache
      */
     public function getIds()
     {
-		$keys = array();
+        $keys = array();
 
-		foreach (glob($this->path . LC_DS . '*.php') as $f) {
-			if ($this->isKeyValid($f)) {
-				$keys[] = substr(basename($f), 0, -4);
-			}
-		}
+        foreach (glob($this->path . LC_DS . '*.php') as $f) {
+            if ($this->isKeyValid($f)) {
+                $keys[] = substr(basename($f), 0, -4);
+            }
+        }
 
         return $keys;
     }
-
-    /**
-     * Delete all cache entries.
-     *
-     * @return array $deleted  Array of the deleted cache ids
-     */
 
     /**
      * Delete all cache entries
@@ -162,87 +156,11 @@ class FileCache extends \Doctrine\Common\Cache\AbstractCache
      * @since  3.0.0
      */
     public function deleteAll()
-	{
+    {
         $keys = array();
 
         foreach (glob($this->path . LC_DS . '*.php') as $f) {
             if (unlink($f)) {
-                $keys[] = substr(basename($f), 0, -4);
-            }
-        }
-
-        return $keys;
-	}
-
-    /**
-     * Delete cache entries where the id matches a PHP regular expressions
-     * 
-     * @param string $regex RegExp pattern
-     *  
-     * @return array Array of the deleted cache ids
-     * @access public
-     * @see    ____func_see____
-     * @since  3.0.0
-     */
-    public function deleteByRegex($regex)
-    {
-		$iterator = new \RegexIterator(
-			new \DirectoryIterator($this->path),
-			$regex,
-			\RegexIterator::GET_MATCH
-		);
-
-		$keys = array();
-
-		foreach ($iterator as $f) {
-			$path = $f->getPath();
-            if (@unlink($path)) {
-                $keys[] = substr(basename($path), 0, -4);
-            }
-		}
-
-		return $keys;
-	}
-
-    /**
-     * Delete cache entries where the id has the passed prefix 
-     * 
-     * @param string $prefix Prefix
-     *  
-     * @return array Array of the deleted cache ids
-     * @access public
-     * @see    ____func_see____
-     * @since  3.0.0
-     */
-    public function deleteByPrefix($prefix)
-    {
-		$keys = array();
-
-        foreach (glob($this->path . LC_DS . $prefix . '*.php') as $f) {
-			if (@unlink($f)) {
-				$keys[] = substr(basename($f), 0, -4);
-			}
-        }
-
-		return $keys;
-	}
-
-    /**
-     * Delete cache entries where the id has the passed suffix
-     * 
-     * @param string $suffix Suffix
-     *  
-     * @return array Array of the deleted cache ids
-     * @access public
-     * @see    ____func_see____
-     * @since  3.0.0
-     */
-    public function deleteBySuffix($suffix)
-    {
-        $keys = array();
-
-        foreach (glob($this->path . LC_DS . '*' . $prefix . '.php') as $f) {
-            if (@unlink($f)) {
                 $keys[] = substr(basename($f), 0, -4);
             }
         }
@@ -262,15 +180,15 @@ class FileCache extends \Doctrine\Common\Cache\AbstractCache
      */
     protected function _doFetch($id)
     {
-		$path = $this->getPathById($id);
+        $path = $this->getPathById($id);
 
-		$result = false;
+        $result = false;
 
-		if (file_exists($path) && $this->isKeyValid($path)) {
-			$result = unserialize(file_get_contents($path, false, null, $this->headerLength + $this->ttlLength));
-		}
+        if (file_exists($path) && $this->isKeyValid($path)) {
+            $result = unserialize(file_get_contents($path, false, null, $this->headerLength + $this->ttlLength));
+        }
 
-		return $result;
+        return $result;
     }
 
     /**
@@ -285,7 +203,7 @@ class FileCache extends \Doctrine\Common\Cache\AbstractCache
      */
     protected function _doContains($id)
     {
-		$path = $this->getPathById($id);
+        $path = $this->getPathById($id);
 
         return file_exists($path) && $this->isKeyValid($path);
     }
@@ -304,12 +222,12 @@ class FileCache extends \Doctrine\Common\Cache\AbstractCache
      */
     protected function _doSave($id, $data, $lifeTime = 0)
     {
-		$lifeTime = strval(min(0, intval($lifeTime)));
+        $lifeTime = strval(min(0, intval($lifeTime)));
 
-		return 0 < file_put_contents(
-			$this->getPathById($id),
-			$this->header . str_repeat(' ', $this->ttlLength - strlen($lifeTime)) . $lifeTime . serialize($data)
-		);
+        return 0 < file_put_contents(
+            $this->getPathById($id),
+            $this->header . str_repeat(' ', $this->ttlLength - strlen($lifeTime)) . $lifeTime . serialize($data)
+        );
     }
 
     /**
@@ -324,52 +242,52 @@ class FileCache extends \Doctrine\Common\Cache\AbstractCache
      */
     protected function _doDelete($id)
     {
-		$path = $this->getPathById($id);
+        $path = $this->getPathById($id);
 
-		$result = false;
+        $result = false;
 
-		if (file_exists($path)) {
-			$result = @unlink($path);
-		}
+        if (file_exists($path)) {
+            $result = @unlink($path);
+        }
 
-		return $result;
+        return $result;
     }
 
-	/**
-	 * Get cell path by cell id 
-	 * 
-	 * @param string $id Cell id
-	 *  
-	 * @return string
-	 * @access protected
-	 * @see    ____func_see____
-	 * @since  3.0.0
-	 */
-	protected function getPathById($id)
-	{
-		return $this->path . LC_DS . $id . '.php';
-	}
+    /**
+     * Get cell path by cell id 
+     * 
+     * @param string $id Cell id
+     *  
+     * @return string
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function getPathById($id)
+    {
+        return $this->path . LC_DS . $id . '.php';
+    }
 
-	/**
-	 * Check - cell file is valid or not
-	 * 
-	 * @param string $path CEll file path
-	 *  
-	 * @return boolean
-	 * @access protected
-	 * @see    ____func_see____
-	 * @since  3.0.0
-	 */
-	protected function isKeyValid($path)
-	{
-		$result = true;
+    /**
+     * Check - cell file is valid or not
+     * 
+     * @param string $path CEll file path
+     *  
+     * @return boolean
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function isKeyValid($path)
+    {
+        $result = true;
 
-		$ttl = intval(file_get_contents($path, false, null, $this->headerLength, $this->ttlLength));
-		if (0 < $ttl && time() > $ttl) {
-			unlink($path);
-			$result = false;
-		}
+        $ttl = intval(file_get_contents($path, false, null, $this->headerLength, $this->ttlLength));
+        if (0 < $ttl && time() > $ttl) {
+            unlink($path);
+            $result = false;
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 }
