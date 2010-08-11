f4395574d8e380855f9dd7647f7b97b9b905a739
max <max@cdev.ru>
2010-08-11 20:02:42 +0000

    [*] Small logic changes in unit test subsystem
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3753 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- .dev/tests/AllTests.php
+++ .dev/tests/AllTests.php
@@ -2,18 +2,28 @@
 // vim: set ts=4 sw=4 sts=4 et:
 
 /**
- * File to run all the tests
- *  
- * @category   X-Lite_Tests
- * @package    X-Lite_Tests
- * @subpackage Main
- * @author     Ruslan R. Fazliev <rrf@x-cart.com> 
- * @copyright  Copyright (c) 2009 Ruslan R. Fazliev <rrf@x-cart.com>
- * @license    http://www.x-cart.com/license.php LiteCommerce license
+ * LiteCommerce
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to licensing@litecommerce.com so we can send you a copy immediately.
+ * 
+ * @category   LiteCommerce
+ * @package    Tests
+ * @subpackage Core
+ * @author     Creative Development LLC <info@cdev.ru> 
+ * @copyright  Copyright (c) 2010 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @version    SVN: $Id$
- * @link       http://www.x-cart.com/
+ * @link       http://www.litecommerce.com/
  * @see        ____file_see____
- * @since      1.0.0
+ * @since      3.0.0
  */
 
 if (false === defined('PHPUnit_MAIN_METHOD')) {
@@ -29,7 +39,6 @@ if (file_exists(PATH_ROOT . '/src')) {
 } else {
     define('PATH_SRC', realpath(PATH_ROOT . '/src'));
 }
-
 
 set_include_path(
     get_include_path()
