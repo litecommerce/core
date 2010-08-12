95dc84f74951f45865092627ea81bd69c38bc05f
vvs <vvs@cdev.ru>
2010-08-12 20:18:56 +0000

    [*] The Model\Repo\Product class is now fully covered by the PHPUnit tests
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3773 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- .dev/tests/Classes/Model/Repo/Product.php
+++ .dev/tests/Classes/Model/Repo/Product.php
@@ -18,6 +18,25 @@
 
 class XLite_Tests_Model_Repo_Product extends XLite_Tests_TestCase
 {
+   /**
+     * Return data needed to start application.
+     * Derived class can redefine this method.
+     * It's possible to detect current test using the $this->name variable
+     *
+     * @return array
+     * @access protected
+     * @see    ____func_see____
+     * @since  1.0.0
+     */
+    protected function getRequest()
+    {
+        $request = parent::getRequest();
+        // Except some ones, all methods are emulate the customer area
+        $request['controller'] = ('testSearchDisabledItems' === $this->name);
+
+        return $request;
+    }
+
     /**
      * setUp 
      * 
@@ -77,43 +96,140 @@ class XLite_Tests_Model_Repo_Product extends XLite_Tests_TestCase
 
 
 	/**
-	 * testsearch 
+	 * testSearchAll
 	 * 
 	 * @return void
 	 * @access public
 	 * @see    ____func_see____
 	 * @since  3.0.0
 	 */
-	public function testsearch1()
+	public function testSearchAll()
 	{
         $cnd = $this->getDefaultCnd();
 
         $result = \XLite\Core\Database::getRepo('\XLite\Model\Product')->search($cnd);
 
-        $this->assertEquals(9, count($result), 'Number of found product does not match');
+        // If all products selected
+        $this->assertEquals(8, count($result), 'Number of found product does not match');
+        // If the first selected (SORT_BY_MODE_NAME, SORT_ORDER_ASC) is the "Apple" one
+        $this->assertEquals(15090, $result[0]->getProductId(), 'ID of the first found product does not match');
 	}
 
     /**
-     * testsearch 
+     * testSearchBySubstring
      * 
      * @return void
      * @access public
      * @see    ____func_see____
      * @since  3.0.0
      */
-    public function testsearch2()
-    {   
+    public function testSearchBySubstring()
+    {
+        $cnd = $this->getDefaultCnd();
+        $cnd->{\XLite\Model\Repo\Product::P_SUBSTRING} = 'botanical';
+
+        $result = \XLite\Core\Database::getRepo('\XLite\Model\Product')->search($cnd);
+
+        // 2 products should be found
+        $this->assertEquals(2, count($result), 'Number of found product does not match');
+        // Check if the "Pea" and "Peach" products are only selected.
+        // Also, the order is checked (SORT_BY_MODE_NAME, SORT_ORDER_ASC)
+        $this->assertEquals(16280, $result[0]->getProductId(), 'ID of the first found product does not match'); // Pea
+        $this->assertEquals(15091, $result[1]->getProductId(), 'ID of the first second product does not match'); // Peach
     }
 
     /**
-     * testsearch 
+     * testSearchInSingleCategory
      * 
      * @return void
      * @access public
      * @see    ____func_see____
      * @since  3.0.0
      */
-    public function testsearch3()
-    {   
+    public function testSearchInSingleCategory()
+    {
+        $cnd = $this->getDefaultCnd();
+        $cnd->{\XLite\Model\Repo\Product::P_CATEGORY_ID} = 14009;
+        $cnd->{\XLite\Model\Repo\Product::P_SEARCH_IN_SUBCATS} = false;
+
+        $result = \XLite\Core\Database::getRepo('\XLite\Model\Product')->search($cnd);
+
+        // 4 products should be found
+        $this->assertEquals(3, count($result), 'Number of found product does not match');
+        // Check category IDs
+        foreach ($result as $entity) {
+            $this->assertEquals(14009, $entity->getCategoryId(), 'Category ID of the product "' . $entity->getName() . '" does not match');
+        }
+    }
+
+    /**
+     * testSearchItemsCount 
+     * 
+     * @return void
+     * @access public
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    public function testSearchItemsCount()
+    {
+        $cnd = $this->getDefaultCnd();
+        $cnd->{\XLite\Model\Repo\Product::P_CATEGORY_ID} = 14015;
+
+        $result = \XLite\Core\Database::getRepo('\XLite\Model\Product')->search($cnd, true);
+
+        // 4 products should be found
+        $this->assertEquals(5, $result, 'Number of found product does not match');
+    }
+
+    /**
+     * testSearchDisabledItems 
+     * NOTE: this method is emulate the admin area
+     * 
+     * @return void
+     * @access public
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    public function testSearchDisabledItems()
+    {
+        $cnd = $this->getDefaultCnd();
+
+        $result = \XLite\Core\Database::getRepo('\XLite\Model\Product')->search($cnd, true);
+
+        // 4 products should be found
+        $this->assertEquals(9, $result, 'Number of found product does not match');
+    }
+
+    /**
+     * testFindByCleanUrlExists 
+     * 
+     * @return void
+     * @access public
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    public function testFindByCleanUrlExists()
+    {
+        $result = \XLite\Core\Database::getRepo('\XLite\Model\Product')->findByCleanUrl('test');
+
+        // Certain product is found
+        $this->assertNotNull($result, 'Product not found');
+        $this->assertEquals(16282, $result->getProductId(), 'Product ID does not match');
+    }
+
+    /**
+     * testFindByCleanUrlNotExists 
+     * 
+     * @return void
+     * @access public
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    public function testFindByCleanUrlNotExists()
+    {
+        $result = \XLite\Core\Database::getRepo('\XLite\Model\Product')->findByCleanUrl('not_exists');
+
+        // Product was not found
+        $this->assertNull($result, 'Wrong result for the search');
     }
 }
