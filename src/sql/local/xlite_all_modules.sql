03bc24f8eb20e921c9d8f42b90a42bcd6637fbbd
svowl <svowl@cdev.ru>
2010-08-15 18:58:54 +0000

    [*] Minor correction in build.xml: the list of enabled modules for build is defined in src/sql/local/xlite_all_modules.sql
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3793 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/sql/local/xlite_all_modules.sql
+++ src/sql/local/xlite_all_modules.sql
@@ -1,4 +1,25 @@
-UPDATE xlite_modules SET enabled=0 WHERE name != 'DrupalConnector';
+UPDATE xlite_modules SET enabled=0;
+UPDATE xlite_modules SET enabled=1 WHERE name IN (
+--'AdvancedSearch',
+--'AuthorizeNet',
+--'Bestsellers',
+'DetailedImages',
+'DrupalConnector',
+--'FeaturedProducts',
+--'GiftCertificates',
+--'GoogleCheckout',
+--'InventoryTracking',
+--'PayPalPro',
+--'ProductAdviser',
+'ProductOptions'
+--'UPSOnlineTools',
+--'USPS',
+--'WishList',
+--'WholesaleTrading',
+--'XPaymentsConnector'
+);
+
+--UPDATE xlite_modules SET enabled=0 WHERE name != 'DrupalConnector';
 
 --UPDATE xlite_modules SET enabled=0;
 --UPDATE xlite_modules SET enabled=1 WHERE NOT name IN (
