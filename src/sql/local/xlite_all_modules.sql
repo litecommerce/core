8537d904b5f9f1c6e1002e45a12fa3bc1add3e97
max <max@cdev.ru>
2010-08-16 05:48:44 +0000

    [*] DetailedImages module is removed
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3795 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/sql/local/xlite_all_modules.sql
+++ src/sql/local/xlite_all_modules.sql
@@ -1,9 +1,8 @@
-UPDATE xlite_modules SET enabled=0;
-UPDATE xlite_modules SET enabled=1 WHERE name IN (
+UPDATE xlite_modules SET enabled = 0;
+UPDATE xlite_modules SET enabled = 1 WHERE name IN (
 --'AdvancedSearch',
 --'AuthorizeNet',
 --'Bestsellers',
-'DetailedImages',
 'DrupalConnector',
 --'FeaturedProducts',
 --'GiftCertificates',
@@ -18,12 +17,3 @@ UPDATE xlite_modules SET enabled=1 WHERE name IN (
 --'WholesaleTrading',
 --'XPaymentsConnector'
 );
-
---UPDATE xlite_modules SET enabled=0 WHERE name != 'DrupalConnector';
-
---UPDATE xlite_modules SET enabled=0;
---UPDATE xlite_modules SET enabled=1 WHERE NOT name IN (
---	'PayPal',
---	'UPS',
---	'Demo'
---);
