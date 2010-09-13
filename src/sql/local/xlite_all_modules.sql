00226dfd78521b9900eb52ee40995da4347b453c
joy <joy@cdev.ru>
2010-09-13 13:54:46 +0000

    [+] PHPUnit tests for Bestsellers module are added
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@3969 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/sql/local/xlite_all_modules.sql
+++ src/sql/local/xlite_all_modules.sql
@@ -2,7 +2,7 @@ UPDATE xlite_modules SET enabled = 0;
 UPDATE xlite_modules SET enabled = 1 WHERE name IN (
 --'AdvancedSearch',
 --'AuthorizeNet',
---'Bestsellers',
+'Bestsellers',
 'DrupalConnector',
 --'FeaturedProducts',
 --'GiftCertificates',
