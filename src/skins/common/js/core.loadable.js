f5766a30acba8649852e5b4f05aa5154bf4cbbb0
joy <joy@cdev.ru>
2010-10-15 07:29:03 +0000

    [*] Common repository has been added
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@4178 194c3f1c-803d-db11-9c9e-000e0c8563f3

---
+++ src/skins/common/js/core.loadable.js
@@ -0,0 +1,327 @@
+/* vim: set ts=2 sw=2 sts=2 et: */
+
+/**
+ * Abstract loadable block (widget)
+ *  
+ * @author    Creative Development LLC <info@cdev.ru> 
+ * @copyright Copyright (c) 2010 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version   SVN: $Id$
+ * @link      http://www.litecommerce.com/
+ * @since     3.0.0
+ */
+
+// Constructor
+function ALoadable(base) {
+  if (base && this.checkBase($(base))) {
+    this.base = $(base);
+
+    var o = this;
+
+    this.loadHandlerCallback = function(xhr, s, data) {
+      return o.loadHandler(xhr, s, data);
+    }
+
+    this.postprocess(true, true);
+  }
+}
+
+extend(ALoadable, Base);
+
+// Base element(s)
+ALoadable.prototype.base = null;
+
+// Shade flag
+ALoadable.prototype.isShowModalScreen = false;
+
+// Widget loading flag
+ALoadable.prototype.isLoading = false;
+
+// Widget has deferred load operation
+ALoadable.prototype.deferredLoad = false;
+
+// Options
+
+// Use shade
+ALoadable.prototype.shadeWidget = true;
+
+// Use widget blocking
+ALoadable.prototype.blockWidget = true;
+
+// Update page title from loaded request or not
+ALoadable.prototype.updatePageTitle = false;
+
+// Widget target
+ALoadable.prototype.widgetTarget = null;
+
+// Widget class name
+ALoadable.prototype.widgetClass = null;
+
+// Page title element pattern
+ALoadable.prototype.titlePattern = 'h1:eq(0)';
+
+// Page title element pattern in request's response data
+ALoadable.prototype.titleRequestPattern = 'h1:eq(0)';
+
+// Container element pattern in request's response data
+ALoadable.prototype.containerRequestPattern = 'div.ajax-container-loadable';
+
+
+// Check base
+ALoadable.prototype.checkBase = function(base)
+{
+  var result = 0 < base.length;
+
+  if (result) {
+    base.map(
+      function() {
+        if (result && 'undefined' != typeof(this.loadable)) {
+          result = false;
+        }
+      }
+    );
+  }
+
+  return result;
+}
+
+// Load widget
+ALoadable.prototype.load = function(params)
+{
+  if (!core.isRequesterEnabled) {
+    return false;
+  }
+
+  if (this.isLoading) {
+    this.deferredLoad = true;
+    return true;
+  }
+
+  this.isLoading = true;
+  this.deferredLoad = false;
+
+  var url = this.buildWidgetRequestURL(params);
+
+  this.base.trigger('preload', [this, url]);
+
+  this.saveState();
+
+  this.shade();
+
+  return core.get(url, this.loadHandlerCallback);
+}
+
+// Build request widget URL (AJAX)
+ALoadable.prototype.buildWidgetRequestURL = function(params)
+{
+  return URLHandler.buildURL(this.getParams(params));
+}
+
+// [ABSTRACT] Get additional parameters
+ALoadable.prototype.getParams = function(params)
+{
+  params = params ? params : {};
+
+  if ('undefined' == typeof(params.target) && this.widgetTarget) {
+    params.target = this.widgetTarget;
+  }
+
+  // TODO remove if it will be no use!!
+  if ('undefined' == typeof(params.sessionCell) && this.sessionCell) {
+    params.sessionCell = this.sessionCell;
+  }
+
+  if ('undefined' == typeof(params.action)) {
+    params.action = '';
+  }
+
+  if ('undefined' == typeof(params.widget) && this.widgetClass) {
+    params.widget = this.widgetClass;
+  }
+
+  return params;
+}
+
+// onload handler
+ALoadable.prototype.loadHandler = function(xhr, s, data)
+{
+  var processed = false;
+
+  this.isLoading = false;
+
+  if (false !== data) {
+    var container = this.getTemporaryContainer();
+    container.html(data);
+    processed = this.placeRequestData(this.extractRequestData(container));
+
+    container.remove();
+  }
+
+  this.unshade();
+
+  this.postprocess(processed);
+
+  if (!this.isLoading && this.deferredLoad) {
+    this.deferredLoad = false;
+    this.load();
+  }
+
+  return processed;
+}
+
+// Get temporary container
+ALoadable.prototype.getTemporaryContainer = function()
+{
+  var div = document.createElement('DIV');
+  div.style.display = 'none';
+  $('body').get(0).appendChild(div);
+  
+  return $(div);
+
+}
+
+// Extract widget data
+ALoadable.prototype.extractRequestData = function(div)
+{
+  return div;
+}
+
+// Place request data
+ALoadable.prototype.placeRequestData = function(box)
+{
+  // Update page title
+  if (this.updatePageTitle) {
+    var title = $(this.titleRequestPattern, box).eq(0);
+    $(this.titlePattern).eq(0).html(title.html());
+  }
+
+  box = $(this.containerRequestPattern, box).children();
+
+  var id = 'temporary-ajax-id-' + (new Date()).getTime();
+
+  box.addClass(id);
+
+  this.base.trigger('reload', [box]);
+  this.base.replaceWith(box);
+  this.base = $('.' + id);
+  this.base.removeClass(id);
+
+  return true;
+}
+
+// [ABSTRACT] Widget post processing (after new widget data placing)
+ALoadable.prototype.postprocess = function(isSuccess, initial)
+{
+  if (isSuccess) {
+    var o = this;
+
+    this.base.map(
+      function() {
+        this.loadable = o;
+      }
+    );
+  }
+}
+
+// [ABSTRACT] Widget save state (before widget load / reload)
+ALoadable.prototype.saveState = function()
+{
+}
+
+// Show modal screen
+ALoadable.prototype.shade = function()
+{
+  if (!this.base || !this.base.length) {
+    return false;
+  }
+
+  if (this.isShowModalScreen) {
+    return true;
+  }
+
+  if (this.shadeWidget)  {
+    this.getShadeBase().block(
+      {
+        message: '<div></div>',
+        css: {
+          width: '30%',
+          top: '35%',
+          left: '35%',
+          zIndex: 16000
+        },
+        overlayCSS: {
+          opacity: 0.1,
+          zIndex: 15000
+        }
+      }
+    );
+
+    $('.blockElement')
+      .css({padding: null, border: null, margin: null, textAlign: null, color: null, backgroundColor: null, cursor: null})
+      .addClass('wait-block');
+
+    $('.blockOverlay')
+      .css({padding: null, border: null, margin: null, textAlign: null, color: null, backgroundColor: null, cursor: null})
+      .addClass('wait-block-overlay');
+
+  } else if (this.blockWidget) {
+
+    this.base.click(this.blocker).focus(this.blocker);
+
+  }
+
+  this.isShowModalScreen = true;
+
+  return true;
+}
+
+// Unshade widget
+ALoadable.prototype.unshade = function()
+{
+  if (!this.base || !this.isShowModalScreen) {
+    return false;
+  }
+
+  if (this.shadeWidget)  {
+    this.getShadeBase().unblock();
+
+  } else if (this.blockWidget) {
+    this.base.unbind('click', this.blocker).unbind('focus', this.blocker);
+  }
+
+  this.isShowModalScreen = false;
+
+  return true;
+}
+
+// Get base element for shade / unshade operation
+ALoadable.prototype.getShadeBase = function() {
+  return this.base;
+}
+
+// Event blocker
+ALoadable.prototype.blocker = function(event) {
+  event.stopPropagation();
+  return false;
+}
+
+// Submit specified POST form
+ALoadable.prototype.submitForm = function(form, callback)
+{
+  var result = false;
+
+  if ('undefined' == typeof(form.validate) || form.validate()) {
+    form = $(form);
+
+    core.post(
+      form.attr('action'),
+      form.serialize(),
+      callback
+    );
+
+    result = true;
+  }
+
+  return result;
+}
+
---
+++ src/skins/common/js/core.popup.js
@@ -0,0 +1,332 @@
+/* vim: set ts=2 sw=2 sts=2 et: */
+
+/**
+ * Popup controller
+ *  
+ * @author    Creative Development LLC <info@cdev.ru> 
+ * @copyright Copyright (c) 2010 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version   SVN: $Id$
+ * @link      http://www.litecommerce.com/
+ * @since     3.0.0
+ */
+
+var popup = {};
+
+/**
+ * Properties
+ */
+
+// Loading status
+popup.isLoading = false;
+
+// Request type status - POST or GET
+popup.isPostRequest = false;
+
+// Current unblock event handler
+popup.currentUnblockCallback = null;
+
+// Identifier of the element displayed in the popup
+popup.elementId = false;
+
+/**
+ * Methods 
+ */
+
+// Load data to popup
+popup.load = function(url, id, unblockCallback, timeout)
+{
+
+  var result = false;
+  if (core.isRequesterEnabled) {
+    var method = null;
+    if (url.constructor == String) {
+      method = 'loadByURL';
+
+    } else if (url.constructor == HTMLFormElement) {
+      method = 'loadByForm';
+
+    } else if (url.constructor == HTMLAnchorElement) {
+      method = 'loadByLink';
+
+    }
+
+    if (method) {
+      this.isLoading = true;
+      this.currentUnblockCallback = unblockCallback;
+      this.openAsWait();
+
+      if (id) {
+        this.elementId = id;
+      }
+      this.isPostRequest = false;
+      result = this[method](url, timeout);
+    }
+  }
+
+  return result;
+}
+
+// Load by URL
+popup.loadByURL = function(url, timeout)
+{
+  return core.get(url, this.postprocessRequestCallback, null, timeout);
+}
+
+// Load by form element
+popup.loadByForm = function(form)
+{
+  form = $(form).eq(0);
+
+  if ('POST' == form.attr('method').toUpperCase()) {
+    this.isPostRequest = true;
+    var result = core.post(form.attr('action'), form.serialize(), this.postprocessRequestCallback);
+
+  } else {
+    var result = core.get(form.attr('action'), this.postprocessRequestCallback, form.serialize());
+  }
+
+  return result;
+}
+
+// Load by link element 
+popup.loadByLink = function(link)
+{
+  link = $(link).eq(0);
+
+  return (1 == link.length && link.attr('href')) ? core.get(link.attr('href'), this.postprocessRequestCallback) : false;
+}
+
+// Postprocess request
+popup.postprocessRequest = function(XMLHttpRequest, textStatus, data, isValid)
+{
+  if (4 != XMLHttpRequest.readyState) {
+
+    // Connection failed
+    this.close();
+
+  } else if (278 == XMLHttpRequest.status) {
+
+    // Redirect
+    this.close();
+    var url = XMLHttpRequest.getResponseHeader('Location');
+    if (url) {
+      self.location = url;
+
+    } else {
+      self.location.reload(true);
+    }
+
+  } else if (279 == XMLHttpRequest.status) {
+
+    // Internal redirect
+    var url = XMLHttpRequest.getResponseHeader('Location');
+    if (url) {
+      this.load(url, this.elementId, this.currentUnblockCallback);
+
+    } else {
+      self.location.reload(true);
+    }
+
+  } else if (277 == XMLHttpRequest.status) {
+
+    // Close popup in silence
+    this.close();
+
+  } else if (200 == XMLHttpRequest.status) {
+
+    // Load new content
+    this.place(data);
+
+  } else {
+
+    // Loading failed
+    this.close();
+
+  }
+}
+
+popup.postprocessRequestCallback = function(XMLHttpRequest, textStatus, data, isValid)
+{
+  return popup.postprocessRequest(XMLHttpRequest, textStatus, data, isValid);
+}
+
+// Place request data
+popup.place = function(data)
+{
+  this.isLoading = false;
+
+  if (false !== data) {
+    data = this.extractRequestData(data);
+    this.open(data);
+  }
+}
+
+// Extract widget data
+popup.extractRequestData = function(data)
+{
+  return data;
+}
+
+// Popup post processing 
+popup.postprocess = function()
+{
+  $('.blockMsg h1#page-title.title').remove();
+
+  var o = this;
+  $('.blockMsg form').submit(
+    function(event) {
+      event.stopPropagation();
+      o.freezePopup();
+      o.load(this, o.elementId, o.currentUnblockCallback);
+      return false;
+    }
+  );
+}
+
+// Freeze popup content
+popup.freezePopup = function()
+{
+  $('.blockMsg form').each(
+    function() {
+      $('button,input:image,input:submit', this).each(
+        function() {
+          if (!this.disabled) {
+            this.temporaryDisabled = true;
+            this.disabled = true;
+        }
+        }
+      );
+    }
+  );
+}
+
+// Unfreeze popup content
+popup.unfreezePopup = function()
+{
+  $('.blockMsg form').each(
+    function() {
+      $('button,input:image,input:submit', this).each(
+        function() {
+          if (this.temporaryDisabled) {
+            this.disabled = false;
+            this.temporaryDisabled = true;
+          }
+        }
+      );
+    }
+  );
+}
+
+// Open as wait box
+popup.openAsWait = function()
+{
+  this.open('<div class="block-wait"></div>');
+}
+
+// Open-n-display popup
+popup.open = function(box)
+{
+  if (box && box.contructor == Object && 'undefined' != typeof(box.html)) {
+    box = box.html();
+  }
+
+  $.blockUI(
+    {
+      message: '<a href="#" class="close-link"></a><div class="block-container"><div class="block-subcontainer">' + box + '</div></div>'
+    }
+  );
+
+  $('.blockMsg').css(
+    {
+      'z-index':  '1200000',
+      'position': 'absolute'
+    }
+  );
+
+  if (this.elementId) {
+    $('.blockMsg').addClass('BlockMsg-' + this.elementId);
+  }
+
+  this.reposition();
+
+  // Add close handler
+  var o = this;
+  $('.blockMsg a.close-link').click(
+    function(event) {
+      o.close();
+      return false;
+    }
+  );
+
+  // Modify overlay
+  $('.blockOverlay')
+    .attr('title', 'Click to unblock')
+    .css(
+      {
+        'z-index':          '1100000',
+        'background-color': 'inherit',
+        'opacity':          'inherit'
+      }
+    )
+    .click(
+      function(event) {
+        return o.close();
+      }
+    );
+
+  this.postprocess();
+}
+
+// Reposition (center) popup
+popup.reposition = function()
+{
+  var w = $(window);
+  var d = $(document);
+  var b = $('.blockMsg');
+
+  var l = Math.max(0, Math.round((w.width() - b.width()) / 2)) + d.scrollLeft();
+  var t = Math.max(0, Math.round((w.height() - b.height()) / 2)) + d.scrollTop();
+
+  $('.blockMsg').css(
+    {
+      'left': l + 'px',
+      'top':  t + 'px'
+    }
+  );
+}
+
+// Close popup
+popup.close = function()
+{
+  $.unblockUI();
+
+  if (this.currentUnblockCallback && this.currentUnblockCallback.constructor == Function) {
+    this.currentUnblockCallback();
+  }
+
+  this.elementId = null;
+  this.currentUnblockCallback = null;
+}
+
+$(document).ready(
+  function() {
+    if ($.blockUI) {
+      $.blockUI.defaults.css =             {};
+      $.blockUI.defaults.centerX =         true;
+      $.blockUI.defaults.centerY =         true;
+      $.blockUI.defaults.bindEvents =      true;
+      $.blockUI.defaults.constrainTabKey = true;
+      $.blockUI.defaults.showOverlay =     true;
+      $.blockUI.defaults.focusInput =      true;
+      $.blockUI.defaults.fadeIn =          0;
+      $.blockUI.defaults.fadeOut =         0;
+    }
+  }
+);
+
+$(window).resize(
+  function(event) {
+    popup.reposition();
+  }
+);
