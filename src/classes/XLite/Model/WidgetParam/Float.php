a1a4c6b33a67136c31ec54d7e229c3ddf0675e77
igoryan <igoryan@cdev.ru>
2010-11-11 14:29:15 +0000

    [*] Dimensional settings related to product image and cloud zoom are now defined as widget params. "Float" widget param class is added.
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@4344 194c3f1c-803d-db11-9c9e-000e0c8563f3

---
+++ src/classes/XLite/Model/WidgetParam/Float.php
@@ -0,0 +1,67 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * LiteCommerce
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to licensing@litecommerce.com so we can send you a copy immediately.
+ * 
+ * @category   LiteCommerce
+ * @package    XLite
+ * @subpackage Model
+ * @author     Creative Development LLC <info@cdev.ru> 
+ * @copyright  Copyright (c) 2010 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version    SVN: $Id$
+ * @link       http://www.litecommerce.com/
+ * @see        ____file_see____
+ * @since      3.0.0
+ */
+
+namespace XLite\Model\WidgetParam;
+
+/**
+ * ____description____
+ * 
+ * @package XLite
+ * @see     ____class_see____
+ * @since   3.0.0
+ */
+class Float extends \XLite\Model\WidgetParam\AWidgetParam
+{
+    /**
+     * Param type
+     *
+     * @var    string
+     * @access protected
+     * @since  3.0
+     */
+    protected $type = 'float';
+
+    /**
+     * Return list of conditions to check
+     *
+     * @param mixed $value value to validate
+     *
+     * @return void
+     * @access protected
+     * @since  3.0.0
+     */
+    protected function getValidaionSchema($value)
+    {
+        return array(
+            array(
+                self::ATTR_CONDITION => !preg_match('/^\s*[-+]?[0-9]*\.?[0-9]+$/Ss', $value),
+                self::ATTR_MESSAGE   => ' is not float',
+            ),
+        );
+    }
+}
