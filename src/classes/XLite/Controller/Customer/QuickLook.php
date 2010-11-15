6da6bc307343bc81ecbcfbd4af09c0011283de93
joy <joy@cdev.ru>
2010-11-15 07:11:40 +0000

    [*] Small code changes.
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@4374 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/classes/XLite/Controller/Customer/QuickLook.php
+++ src/classes/XLite/Controller/Customer/QuickLook.php
@@ -31,9 +31,9 @@ namespace XLite\Controller\Customer;
 /**
  * QuickLook 
  * 
- * @package    XLite
- * @see        ____class_see____
- * @since      3.0.0
+ * @package XLite
+ * @see     ____class_see____
+ * @since   3.0.0
  */
 class QuickLook extends \XLite\Controller\Customer\Product
 {
