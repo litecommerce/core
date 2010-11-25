7dfb561e8b0ade2beeeaf885bd3d05247fc09499
svowl <svowl@cdev.ru>
2010-11-25 18:43:47 +0000

    [*] Demo module updated: registering of config.demo.php moved to ..\Demo\Core\Database class from Main [!] Demo SQL dump corrected: inserts into category_products moved at the bottom of sql/demo/xlite_demo_store.sql
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@4539 194c3f1c-803d-db11-9c9e-000e0c8563f3

---
+++ src/classes/XLite/Module/Demo/Core/Database.php
@@ -0,0 +1,50 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * LiteCommerce
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to licensing@litecommerce.com so we can send you a copy immediately.
+ * 
+ * @category   LiteCommerce
+ * @package    XLite
+ * @subpackage Controller
+ * @author     Creative Development LLC <info@cdev.ru> 
+ * @copyright  Copyright (c) 2010 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version    SVN: $Id$
+ * @link       http://www.litecommerce.com/
+ * @see        ____file_see____
+ * @since      3.0.0
+ */
+
+namespace XLite\Module\Demo\Core\Database;
+
+class Database extends \XLite\Core\Database implements \XLite\Base\IDecorator
+{
+
+    /**
+     * connect
+     *
+     * @return boolean 
+     * @access public
+     * @since  3.0.0
+     */
+    public function connect()
+    {
+        if (\XLite::getInstance()->isAdminZone()) {
+            \Includes\Utils\ConfigParser::registerConfigFile('config.demo.php');
+        }
+
+        parent::connect();
+    }
+}
+
