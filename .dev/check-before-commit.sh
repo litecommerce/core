eb639a3621f0caa22d596399c379e86bbdca29cd
max <max@cdev.ru>
2010-11-25 14:21:46 +0000

    [*] Service script check-before-commit is added
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@4531 194c3f1c-803d-db11-9c9e-000e0c8563f3

---
+++ .dev/check-before-commit.sh
@@ -0,0 +1,12 @@
+cd $(dirname $0)
+cd ../
+
+for i in `svn status src/classes | sed -E 's/^[A-Z] +//'`;
+do
+	.dev/phpcs-report.sh $i | grep -v '^Standard: ';
+	/usr/local/php-530/bin/php -l $i | grep -v '^No syntax ';
+
+	testPath=`echo $i | sed -E 's/src.classes.XLite.//' | sed -E 's/.php//'`;
+	.dev/phpunit.sh $testPath | sed -E '/^DB |^PHPUnit |^Time: |^OK |^$|\.\.\.\.\.\.$/D'
+
+done
