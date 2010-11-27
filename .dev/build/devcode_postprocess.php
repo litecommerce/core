26ef213af400d11055d9e677164d0b06af9269a1
svowl <svowl@cdev.ru>
2010-11-27 17:48:16 +0000

    [*] release.sh updated: devcode_postprocess.php call is added [*] Quantum module removed from demo version
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@4552 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- .dev/build/devcode_postprocess.php
+++ .dev/build/devcode_postprocess.php
@@ -4,28 +4,35 @@
 if (php_sapi_name() != 'cli')
 	exit(1);
 
+$silentMode = false;
+
+if (empty($argv) && false !== array_search('silentMode', array_keys($argv))) {
+	$silentMode = true;
+}
+
 $files = getPHPs('./');
 
 echo "Trim developer blocks / files post-processing...\n";
 
 foreach ($files as $f) {
 	$f = substr($f, 2);
-	echo $f . '... ';
 
 	$data = file_get_contents($f);
 
 	if (preg_match('/@package\s+DEVCODE/Ss', $data)) {
 		unlink($f);
-		echo "removed\n";
+		if (!$silentMode) {
+			echo $f . "...removed\n";
+		}
 		continue;
 	}
 
 	$data = preg_replace("/\n[ ]*\/\/\s+DEVCODE.+\/\/\s+\/DEVCODE\s/USs", '', $data);
 
 	file_put_contents($f, $data);
-
-	echo "done\n";
 }
+
+echo "    done\n";
 
 /*
 	Service functions
