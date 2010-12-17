fa4033e4f792eb371f318a0bc052b3ac2a89431e
igoryan <igoryan@cdev.ru>
2010-12-17 10:29:57 +0000

    [!] Fixed double-escaping problems in standalone mode (and administrator zone).
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@4719 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/classes/XLite/Core/Request.php
+++ src/classes/XLite/Core/Request.php
@@ -106,13 +106,15 @@ class Request extends \XLite\Base\Singleton
         if (isset($data['action'])) {
             $this->checkControlArgument($data['action'], 'Action');
         }
-/*
-        // FIXME: this code is needed, but brakes down the current ajax functionality
-        //        Fix ajax and tests to work with this
-        if (1 === get_magic_quotes_gpc()) {
+
+        // Fix double-escaping problems caused by "magic quotes" for a stand-alone mode
+        // and admin side
+        if (
+            !\XLite\Module\CDev\DrupalConnector\Handler::getInstance()->checkCurrentCMS()
+            && 1 === get_magic_quotes_gpc() 
+        ) {
             $data = $this->doUnescape($data);
         }
-*/
 
         return $data;
     }
