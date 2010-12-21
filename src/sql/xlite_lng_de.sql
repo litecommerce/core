c1ec7c399d0b524a2be6c8caafd7d22093630292
max <max@cdev.ru>
2010-12-21 08:07:23 +0000

    M:38786 [-] All obsolete SQL dump all removed
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@4734 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/sql/xlite_lng_de.sql
+++
@@ -1,4 +0,0 @@
--- Language labels
-INSERT INTO xlite_language_label_translations (id, code, label) VALUES (1, 'de', 'Gewicht');
-INSERT INTO xlite_language_label_translations (id, code, label) VALUES (52, 'de', 'Suche');
-
