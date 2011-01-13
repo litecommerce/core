0dfa890a29ea19b96ef8b15c30ea9109607f3509
vvs <vvs@cdev.ru>
2011-01-13 18:36:52 +0000

    E:0038801 [*] Resources registration subsystem for Drupal has been refactored
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@4885 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/classes/XLite/Module/CDev/DrupalConnector/Drupal/ADrupal.php
+++ src/classes/XLite/Module/CDev/DrupalConnector/Drupal/ADrupal.php
@@ -48,6 +48,19 @@ abstract class ADrupal extends \XLite\Base\Singleton
     protected $handler;
 
     /**
+     * Already registered resources
+     *
+     * @var    array
+     * @access protected
+     * @see    ____var_see____
+     * @since  3.0.0
+     */
+    protected static $registeredResources = array('js' => array(), 'css' => array());
+
+
+    // ------------------------------ Application layer -
+
+    /**
      * Return instance of current CMS connector
      * 
      * @return \XLite\Module\CDev\DrupalConnector\Handler
@@ -83,5 +96,160 @@ abstract class ADrupal extends \XLite\Base\Singleton
 
         $this->getHandler()->mapRequest(array(\XLite\Core\CMSConnector::NO_REDIRECT => true) + $data);
         $this->getHandler()->runController(md5(serialize($data)));
+    }
+
+
+    // ------------------------------ Resources (CSS and JS) -
+
+    /**
+     * Get resources (from list) which are not already registered
+     * 
+     * @param string $type  Resource type ("js" or "css")
+     * @param array  $files Resource files
+     *  
+     * @return array
+     * @access protected
+     * @see    ____func_see____
+     i* @since  3.0.0
+     */
+    protected function getUniqueResources($type, array $files)
+    {
+        $files = array_diff($files, static::$registeredResources[$type]);
+        static::$registeredResources[$type] = array_merge(static::$registeredResources[$type], $files);
+
+        return $files;
+    }
+
+    /**
+     * Get JS scope
+     *
+     * @param string $file Resource file path
+     *
+     * @return string
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function getJSScope($file)
+    {
+        return preg_match('/.skins.common.js./Ss', $file) ? 'header' : 'footer';
+    }
+
+    /**
+     * Get file unique basename
+     *
+     * @param string $file Resource file path
+     *
+     * @return string
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function getResourceBasename($file)
+    {
+        return preg_replace('/\.(css|js)$/Ss', '.' . uniqid() . '.$1', basename($file));
+    }
+
+    /**
+     * Get resource description in Drupal format
+     *
+     * @param string $file Resource file path
+     *
+     * @return null
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function getResourceInfoCommon($file)
+    {
+        return array(
+            'type'     => 'file',
+            'basename' => $this->getResourceBasename($file),
+        );
+    }
+
+    /**
+     * Get resource description in Drupal format
+     *
+     * @param string $file Resource file path
+     *
+     * @return null
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function getResourceInfoJS($file)
+    {
+        return array(
+            'scope' => $this->getJSScope($file),
+        );
+    }
+
+    /**
+     * Get resource description in Drupal format
+     *
+     * @param string $file Resource file path
+     *
+     * @return null
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function getResourceInfoCSS($file)
+    {
+        return array(
+            'group' => CSS_DEFAULT,
+        );
+    }
+
+    /**
+     * Get resource description in Drupal format
+     * 
+     * @param string $type Resource type ("js" or "css")
+     * @param string $file Resource file path
+     *  
+     * @return null
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function getResourceInfo($type, $file)
+    {
+        return $this->getResourceInfoCommon($file) + $this->{__FUNCTION__ . strtoupper($type)}($file);
+    }
+
+    /**
+     * Register single resource
+     *
+     * @param string $type Resource type ("js" or "css")
+     * @param string $file Resource file path
+     *
+     * @return mixed
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function registerResource($type, $file)
+    {
+        return call_user_func_array('drupal_add_' . $type, array($file, $this->getResourceInfo($type, $file)));
+    }
+
+    /**
+     * Register LC widget resources
+     *
+     * @param \XLite\View\AView $widget LC widget to get resources list
+     *
+     * @return null
+     * @access protected
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function registerResources(\XLite\View\AView $widget)
+    {
+        foreach ($widget->getRegisteredResources() as $type => $files) {
+            foreach ($this->getUniqueResources($type, $files) as $file) {
+                $this->registerResource($type, $file);
+            }
+        }
     }
 }
