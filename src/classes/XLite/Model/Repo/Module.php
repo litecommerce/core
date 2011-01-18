4c3f10239046efe87fa21d725421e1101abb2497
igoryan <igoryan@cdev.ru>
2011-01-18 08:10:09 +0000

    [*] Logic changes in the logging during modules update procedure.
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@4910 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/classes/XLite/Model/Repo/Module.php
+++ src/classes/XLite/Model/Repo/Module.php
@@ -897,15 +897,12 @@ class Module extends \XLite\Model\Repo\ARepo
         if ($this->updateError) {
             \XLite\Logger::getInstance()->log(
                 'The following error occured during add-ons update procedure:'
-                . PHP_EOL . $this->updateError
+                . PHP_EOL . $this->updateError,
+                LOG_ERR
             );
             \XLite\Core\TopMessage::getInstance()->addWarning(
                 'Error occured when getting addons data from the LiteCommerce Market place. '
                 . 'Check log file for details.'
-            );
-        } else {
-            \XLite\Logger::getInstance()->log(
-                'Add-ons data has been successfully updated from the Market place'
             );
         }
 
