bccca84529570006f518aacdff664472c5db3360
igoryan <igoryan@cdev.ru>
2011-01-21 11:21:26 +0000

    [*] Tests on a Module model are partially added.
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@4941 194c3f1c-803d-db11-9c9e-000e0c8563f3

---
+++ .dev/tests/Classes/Model/Repo/Module.php
@@ -0,0 +1,92 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * LiteCommerce
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to licensing@litecommerce.com so we can send you a copy immediately.
+ * 
+ * @category   LiteCommerce
+ * @package    Tests
+ * @subpackage Classes
+ * @author     Creative Development LLC <info@cdev.ru> 
+ * @copyright  Copyright (c) 2010 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version    SVN: $Id$
+ * @link       http://www.litecommerce.com/
+ * @see        ____file_see____
+ * @since      3.0.0
+ */
+
+class XLite_Tests_Model_Repo_Module extends XLite_Tests_Model_ModuleAbstract
+{
+    public function testSearch
+    {
+    }
+
+    public function testFindAllModules
+    {
+    }
+
+    public function testFindInactiveModules
+    {
+    }
+
+    public function testFindUpgradableModules
+    {
+    }
+
+    public function testFindAllNames
+    {
+    }
+
+    public function testFindAllByModuleIds
+    {
+    }
+
+    public function testFindAllEnabled
+    {
+    }
+
+    public function testFindAllByNames
+    {
+    }
+
+    public function testFindByActualName
+    {
+    }
+
+    public function testGetActiveModules
+    {
+    }
+
+    public function testIsModuleActive
+    {
+    }
+
+    public function testInitialize
+    {
+    }
+
+    public function testCheckModules
+    {
+    }
+
+    public function test
+    {
+    }
+
+    public function test
+    {
+    }
+
+
+}
