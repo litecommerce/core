bd2c9c4c4804a227f6f570325e3ef7ef3a9b64e9
svowl <svowl@cdev.ru>
2011-01-23 17:32:20 +0000

    [*] The list of directories-to-create (install_settings.php) is empty now.
    git-svn-id: svn://svn.crtdev.local/repo/xlite/main@4957 194c3f1c-803d-db11-9c9e-000e0c8563f3

--- src/Includes/install/install_settings.php
+++ src/Includes/install/install_settings.php
@@ -91,8 +91,6 @@ $lcSettings = array(
 
     // The list of directories that should be created by installation script
     'directories_to_create' => array(
-        'var/backup',
-        'var/html',
     ),
 
     // The list of files that should be created by installation script
