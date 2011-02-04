4e09bb28b651177ca2622ad5d057465c9dbcafa0
Maxim Shamaev <max@cdev.ru>
2011-02-04 15:40:58 +0300

    [*] Small changes in Selenium-based tests subsystem

--- .dev/tests/local-cc.php
+++ .dev/tests/local-cc.php
@@ -1,6 +1,7 @@
 <?php
 
 define('SELENIUM_SOURCE_URL', 'http://xcart2-530.crtdev.local/~xcart/general/projects/xlite/build');
+define('SELENIUM_COVERAGE_URL', 'http://xcart2-530.crtdev.local/~xcart/general/projects/xlite/build');
 
 define('DRUPAL_SITE_PATH', '/u/xcart/public_html/general/projects/xlite/build');
 
