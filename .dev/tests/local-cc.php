67e069526e3e1875b5b81a9e3e75df5a7fc2eebb
Maxim Muhin <joy@xcart.crtdev.local>
2011-02-24 13:50:17 +0300

    [!] Bug: SELENIUM_SOURCE_URL_ADMIN constant was not defined in test installation

--- .dev/tests/local-cc.php
+++ .dev/tests/local-cc.php
@@ -1,6 +1,7 @@
 <?php
 
 define('SELENIUM_SOURCE_URL', 'http://xcart2-530.crtdev.local/~xcart/general/projects/xlite/build/src');
+define('SELENIUM_SOURCE_URL_ADMIN', 'http://xcart2-530.crtdev.local/~xcart/general/projects/xlite/build/src/modules/lc_connector/litecommerce/');
 define('SELENIUM_COVERAGE_URL', 'http://xcart2-530.crtdev.local/~xcart/general/projects/xlite/build');
 
 define('DRUPAL_SITE_PATH', '/u/xcart/public_html/general/projects/xlite/build/src');
