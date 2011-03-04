4ddf199e07c1441650c537028af919aed002c6c2
Vladimir Semenov <svowl@cdev.ru>
2011-03-04 16:19:29 +0300

    E:0039252 [*] Added a possibility to change 'access_level' and 'status' users profile fields by Drupal administrator

--- src/classes/XLite/Module/CDev/DrupalConnector/View/Model/Profile/Main.php
+++ src/classes/XLite/Module/CDev/DrupalConnector/View/Model/Profile/Main.php
@@ -55,4 +55,55 @@ class Main extends \XLite\View\Model\Profile\Main implements \XLite\Base\IDecora
             parent::postprocessErrorActionValidateInput();
         }
     }
+
+    /**
+     * Perform certain action for the model object
+     *
+     * @return boolean 
+     * @access protected
+     * @since  3.0.0
+     */
+    protected function performActionValidateInput()
+    {
+        $result = parent::performActionValidateInput();
+
+        // Success validation if controller is launched from Drupal context by an administrator
+        if (!$result && \XLite\Module\CDev\DrupalConnector\Handler::getInstance()->checkCurrentCMS()) {
+            $result = \XLite\Core\Auth::getInstance()->isAdmin();
+        }
+
+        return $result;
+    }
+
+    /**
+     * Make 'Access level' field available when administrator modifies other user's profile
+     * 
+     * @param array &$data Widget params
+     *  
+     * @return void
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function prepareFieldParamsAccessLevel(&$data)
+    {
+        if (\XLite\Core\Auth::getInstance()->isAdmin()) {
+            $data[\XLite\View\FormField\AFormField::PARAM_IS_ALLOWED_FOR_CUSTOMER] = true;
+        }
+    }
+
+    /**
+     * Make 'Status' field available when administrator modifies other user's profile
+     * 
+     * @param array &$data Widget params
+     *  
+     * @return void
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function prepareFieldParamsStatus(&$data)
+    {
+        if (\XLite\Core\Auth::getInstance()->isAdmin()) {
+            $data[\XLite\View\FormField\AFormField::PARAM_IS_ALLOWED_FOR_CUSTOMER] = true;
+        }
+    }
 }
