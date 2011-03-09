3283683b44628a21d3676c896baba69ed6a87838
Vladimir Semenov <svowl@cdev.ru>
2011-03-09 14:45:20 +0300

    E:0039252 [*] Functional tests added on profiles manipulations on Drupal side

--- .dev/tests/Web/Customer/UserDetails.php
+++ .dev/tests/Web/Customer/UserDetails.php
@@ -28,55 +28,256 @@
 
 class XLite_Web_Customer_UserDetails extends XLite_Web_Customer_ACustomer
 {
-    protected function login($user = 'master', $password = 'master')
+    /**
+     * Users data for testing
+     * 
+     * @var    array
+     * @see    ____var_see____
+     * @since  3.0.0
+     */
+    protected $users = array(
+        // Administrator
+        1 => array(
+            'login'    => 'master',
+            'password' => 'master', // md5(master) = eb0a191797624dd3a48fa681d3061212
+            'email'    => 'rnd_tester@cdev.ru',
+        ),
+        // Customer
+        2 => array(
+            'login'    => 'user2011',
+            'password' => 'demo', // md5(demo) = fe01ce2a7fbac8fafaed7c982a04e229
+            'email'    => 'rnd_tester05@cdev.ru',
+        ),
+    );
+
+
+    /**
+     * Test on simple update own profile by administrator
+     * 
+     * @return void
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    public function testUpdate1()
     {
-        $this->open('user');
-
-        $this->type('css=#edit-name', 'master');
-        $this->type('css=#edit-pass', 'master');
-
-        $this->submitAndWait('css=#user-login');
+        $user = $this->getUser(1);
+        
+        $this->loginUser($user);
 
         $this->open('user/1/edit');
+
+        $this->clickAndWait('css=#edit-submit');
+
+        if ($this->isElementPresent('//div[@id="console"]/div[@class="messages error"]')) {
+            $message = $this->getText('//div[@id="console"]/div[@class="messages error"]');
+            $this->assertNull($message, 'Check for error messages');
+        }
     }
 
-    public function testUpdate()
+    /**
+     * Test on update own profile by administrator with modification of some data
+     * 
+     * @return void
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    public function testUpdate2()
     {
-        $this->login();
+        $user = $this->getUser(1);
+        
+        $this->loginUser($user);
+
+        $this->open('user/1/edit');
 
         $email = 'rnd_tester' . time() . '@cdev.ru';
 
-        // Change
-        $this->type('css=#edit-pass-pass1', 'master');
-        $this->type('css=#edit-pass-pass2', 'master');
-        $this->typeKeys(
-            'css=#edit-mail',
-            $email
-        );
+        // Specify current password
+        $this->type('css=#edit-current-pass', $user['password']);
+
+        // TODO: uncomment password changing after this will be fixed
+
+        // Change password
+        //$this->type('css=#edit-pass-pass1', $user['password']);
+        //$this->type('css=#edit-pass-pass2', $user['password']);
+
+        // Change email
+        $this->type('css=#edit-mail', $email);
 
         $this->clickAndWait('css=#edit-submit');
 
+        if ($this->isElementPresent('//div[@id="console"]/div[@class="messages error"]')) {
+            $message = $this->getText('//div[@id="console"]/div[@class="messages error"]');
+            $this->assertNull($message, 'Check for error messages #1');
+        }
+
+        // Check that email is modified successfully 
         $this->assertEquals(
             $email,
             $this->getJSExpression('jQuery("#edit-mail").val()'),
-            'check changed email'
+            'Checking changed email'
         );
-        $this->assertJqueryNotPresent('#status-messages ul li.error', 'check errors');
 
-        // Revert
-        $this->type('css=#edit-pass-pass1', 'master');
-        $this->type('css=#edit-pass-pass2', 'master');
-        $this->typeKeys(
-            'css=#edit-mail',
-            'rnd_tester@cdev.ru'
-        );
+        // Revert changes back
+
+        // Specify current password
+        $this->type('css=#edit-current-pass', $user['password']);
+
+        //$this->type('css=#edit-pass-pass1', $user['password']);
+        //$this->type('css=#edit-pass-pass2', $user['password']);
+        $this->type('css=#edit-mail', $user['email']);
 
         $this->clickAndWait('css=#edit-submit');
+
+        if ($this->isElementPresent('//div[@id="console"]/div[@class="messages error"]')) {
+            $message = $this->getText('//div[@id="console"]/div[@class="messages error"]');
+            $this->assertNull($message, 'Check for error messages #2');
+        }
+
+        // Check that email is modified successfully 
+        $this->assertEquals(
+            $user['email'],
+            $this->getJSExpression('jQuery("#edit-mail").val()'),
+            'Check reverted email'
+        );
     }
 
+    /**
+     * Test on user creation, then test on update this user's profile by the administrator and created user
+     * 
+     * @return void
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    public function testCreateUser1()
+    {
+        $user = $this->getUser(1);
+
+        $this->loginUser($user);
+
+        // Create new user with an administrator permissions and disabled status
+
+        $this->open('admin/people/create');
+
+        $user2 = $this->getUser(2);
+
+        //Fill profile details form 
+        $this->type('css=#edit-name', $user2['login']);
+        $this->type('css=#edit-mail', $user2['email']);
+        $this->type('css=#edit-pass-pass1', $user2['password']);
+        $this->type('css=#edit-pass-pass2', $user2['password']);
+
+        $this->check('css=#edit-status-0'); // User status is blocked 
+        $this->check('css=#edit-roles-3'); // User role - administrator
+
+        $this->clickAndWait('css=#edit-submit');
+
+        if ($this->isElementPresent('//div[@id="console"]/div[@class="messages error"]')) {
+            $message = $this->getText('//div[@id="console"]/div[@class="messages error"]');
+            $this->assertNull($message, 'Check for error messages #1');
+        }
+
+        $this->assertElementPresent(
+            '//div[@id="console"]/div[@class="messages status"]//descendant::a/em[text()="' . $user2['login']. '"]',
+            'Check that link to created user profile is presented'
+        );
+
+        $linkHref = $this->getJSExpression('jQuery("#console a").attr("href")');
+
+        $this->assertNotNull($linkHref, 'Check that href of link to the profile is not null');
+
+        if (preg_match('/user\/(\d+)/', $linkHref, $match)) {
+            $userId = $match[1];
+        }
+
+        $this->assertTrue(intval($userId) == $userId, 'Check that $userId value is integer (' . $userId . ')');
+
+        $newProfile = \XLite\Core\Database::getRepo('XLite\Model\Profile')->findOneBy(array('cms_profile_id' => $userId));
+
+        $this->assertNotNull($newProfile, 'Check that new profile is not null');
+
+        $this->assertEquals($user2['email'], $newProfile->getLogin(), 'Check that email/login of new user profile in LC is the same as in Drupal');
+        $this->assertTrue($newProfile->isAdmin(), 'Check that new user is LC administrator');
+        $this->assertFalse($newProfile->isEnabled(), 'Check that new user account is disabled in LC');
+
+        $profileId = $newProfile->getProfileId();
+
+        // Edit created user: activate it and remove administrator permissions
+
+        $this->open('user/' . $userId . '/edit');
+
+        $this->check('css=#edit-status-1'); // User status is active 
+        $this->uncheck('css=#edit-roles-3'); // Reset user role - administrator
+
+        $this->clickAndWait('css=#edit-submit');
+
+        if ($this->isElementPresent('//div[@id="console"]/div[@class="messages error"]')) {
+            $message = $this->getText('//div[@id="console"]/div[@class="messages error"]');
+            $this->assertNull($message, 'Check for error messages #2');
+        }
+
+        // Detach object to get this one again from database
+        $newProfile->detach();
+
+        $newProfile = \XLite\Core\Database::getRepo('XLite\Model\Profile')->find($profileId);
+
+        $this->assertNotNull($newProfile, 'Check that new profile is not null');
+
+        $this->assertEquals($user2['email'], $newProfile->getLogin(), 'Check that email/login of new user profile in LC is the same as in Drupal');
+        $this->assertFalse($newProfile->isAdmin(), 'Check that new user is not LC administrator (profile_id = ' . $profileId . ', status = ' . $newProfile->getAccessLevel() . ')');
+        $this->assertTrue($newProfile->isEnabled(), 'Check that new user account is enabled in LC (profile_id = ' . $profileId . ')');
+
+        // Log in as new user and update profile
+
+        $user = $this->getUser(2);
+
+        $this->loginUser($user);
+
+        $this->open('user/' . $userId . '/edit');
+
+        $email = 'rnd_tester' . time() + 1000 . '@cdev.ru';
+        $newPassword = 'newpassword';
+
+        // Specify current password
+        $this->type('css=#edit-current-pass', $user['password']);
+
+        // TODO: uncomment password changing after this will be fixed
+
+        // Change password
+        //$this->type('css=#edit-pass-pass1', $newPassword);
+        //$this->type('css=#edit-pass-pass2', $newPassword);
+
+        // Change email
+        $this->type('css=#edit-mail', $email);
+
+        $this->clickAndWait('css=#edit-submit');
+
+        if ($this->isElementPresent('//div[@id="console"]/div[@class="messages error"]')) {
+            $message = $this->getText('//div[@id="console"]/div[@class="messages error"]');
+            $this->assertNull($message, 'Check for error messages');
+        }
+
+        // Check that email is modified successfully 
+        $this->assertEquals(
+            $email,
+            $this->getJSExpression('jQuery("#edit-mail").val()'),
+            'Checking changed email'
+        );
+    }
+
+    /**
+     * Test on password complexity checking
+     * 
+     * @return void
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
     public function testPassword()
     {
-        $this->login();
+        $user = $this->getUser(1);
+        
+        $this->loginUser($user);
+
+        $this->open('user/1/edit');
 
         // Check password strength
         $this->typeKeys(
@@ -145,5 +346,36 @@ class XLite_Web_Customer_UserDetails extends XLite_Web_Customer_ACustomer
         $this->clickAndWait('css=#edit-submit');
 
         $this->assertJqueryPresent('.messages.error h2', 'check errors');
+    }
+
+
+    /**
+     * Return specified user data from an array $users
+     * 
+     * @param integer $id User index in the $users array
+     *  
+     * @return void
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function getUser($id)
+    {
+        return $this->users[$id];
+    }
+
+    /**
+     * Log in user with specified data
+     * 
+     * @param array $user Cell of $users array
+     *  
+     * @return void
+     * @see    ____func_see____
+     * @since  3.0.0
+     */
+    protected function loginUser($user)
+    {
+        $this->logIn($user['login'], $user['password']);
+
+        // TODO: add checking if profile in Drupal is synchronized with profile in LC
     }
 }
