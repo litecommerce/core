b67df4c5cc91760f5998ed6a2923e61f5355beef
Vladimir Semenov <svowl@cdev.ru>
2011-03-22 10:25:50 +0300

    E:0039322 [*] Some improvements in core of tests

--- .dev/tests/local-cc.php
+++ .dev/tests/local-cc.php
@@ -1,16 +1,22 @@
 <?php
 
 // Directory where xlite-tests.config.php is located
-define('CONFIG_DIR', '/u/xcart/etc');
+define('XLITE_DEV_CONFIG_DIR', '/u/xcart/etc');
+
+// Directory with external libs (PHPUnit etc)
+define('XLITE_DEV_LIB_DIR', '/u/xcart/lib');
+
 
 // URL of LiteCommerce store (path only, w/o script)
 define('SELENIUM_SOURCE_URL_ADMIN', 'http://xcart2-530.crtdev.local/~xcart/general/projects/xlite/build/src/modules/lc_connector/litecommerce/');
 
-// Settings for web tests
+// URL of Drupal+LC store
 define('SELENIUM_SOURCE_URL', 'http://xcart2-530.crtdev.local/~xcart/general/projects/xlite/build/src');
+
+// Directory where Drupal installed
 define('DRUPAL_SITE_PATH', '/u/xcart/public_html/general/projects/xlite/build/src');
 
-// Settings for screenshots capturing
+// Settings for screenshots capturing (on failed tests)
 define('SELENIUM_SCREENSHOTS_PATH', 'C:\\Inetpub\\wwwroot\\selenium-screenshots');
 define('SELENIUM_SCREENSHOTS_URL', 'http://cormorant.crtdev.local/selenium-screenshots');
 
