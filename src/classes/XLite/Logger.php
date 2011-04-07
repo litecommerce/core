6130bc4ae4009acc0b8c758dae8e2cca567f5947
Vladimir Semenov <svowl@cdev.ru>
2011-04-07 15:54:19 +0400

    E:0039430 [!] Wrong directory var/log created. Fixed

--- src/classes/XLite/Logger.php
+++ src/classes/XLite/Logger.php
@@ -340,7 +340,7 @@ class Logger extends \XLite\Base\Singleton
         $result = $this->options['name'];
 
         if ('file' == $this->getType()) {
-            $dir = dirname($result);
+            $dir = dirname(LC_DIR . ltrim($result, LC_DS));
             $file = basename($result);
             $parts = explode('.', $file);
             array_splice($parts, count($parts) - 1, 0, date('Y-m-d'));
