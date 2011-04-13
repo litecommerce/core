05272e48ee0f3e564bab861a84266ff6a0fc3301
Maxim Muhin <joy@xcart.crtdev.local>
2011-04-13 15:22:33 +0400

    [*] Small design changes in TinyMCE start options.

--- src/skins/admin/en/modules/CDev/TinyMCE/js/script.js
+++ src/skins/admin/en/modules/CDev/TinyMCE/js/script.js
@@ -15,7 +15,8 @@ $(function() {
   $('textarea.tinymce').tinymce({
 
     // General options
-    theme : "advanced",
+    theme : "advanced", // Use Advanced theme
+
     plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
 
     // Theme options
@@ -28,7 +29,10 @@ $(function() {
     theme_advanced_toolbar_location : "top",
     theme_advanced_toolbar_align : "left",
     theme_advanced_statusbar_location : "bottom",
-    theme_advanced_resizing : true
+    theme_advanced_resizing : true,
+
+    // Prevents automatic converting URLs to relative ones.
+    convert_urls : false
 
   });
 });
