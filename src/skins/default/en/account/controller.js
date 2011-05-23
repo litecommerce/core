ffe1ed0ddaa03b61ffe58432ebcaaed575130221
Maxim Shamaev <max@cdev.ru>
2011-05-23 19:42:40 +0400

    E:39375 [*] Add redirect to HTTPS for controllers

--- src/skins/default/en/account/controller.js
+++ src/skins/default/en/account/controller.js
@@ -6,7 +6,7 @@
  * @author    Creative Development LLC <info@cdev.ru>
  * @copyright Copyright (c) 2011 Creative Development LLC <info@cdev.ru>. All rights reserved
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @version   SVN: $Id$
+ * @version   SVN: $Id: e3e67d55a9dcb5b8033e2485a05aab24ffeb384d $
  * @link      http://www.litecommerce.com/
  * @since     1.0.0
  */
