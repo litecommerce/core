1c699a892fd43d831fe1a4cf71327b785a9ef58f
Vadim Sannikov <vvs@cdev.ru>
2011-05-26 23:35:21 +0400

    S:127623 [*] Fixes and improvements for the modules installation

--- src/classes/XLite/Module/CDev/FeaturedProducts/Main.php
+++ src/classes/XLite/Module/CDev/FeaturedProducts/Main.php
@@ -3,9 +3,9 @@
 
 /**
  * LiteCommerce
- *
+ * 
  * NOTICE OF LICENSE
- *
+ * 
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
@@ -13,16 +13,16 @@
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to licensing@litecommerce.com so we can send you a copy immediately.
- *
- * @category   LiteCommerce
- * @package    XLite
- * @subpackage ____sub_package____
- * @author     Creative Development LLC <info@cdev.ru>
- * @copyright  Copyright (c) 2011 Creative Development LLC <info@cdev.ru>. All rights reserved
- * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link       http://www.litecommerce.com/
- * @see        ____file_see____
- * @since      1.0.0
+ * 
+ * PHP version 5.3.0
+ * 
+ * @category  LiteCommerce
+ * @author    Creative Development LLC <info@cdev.ru> 
+ * @copyright Copyright (c) 2011 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.litecommerce.com/
+ * @see       ____file_see____
+ * @since     1.0.0
  */
 
 namespace XLite\Module\CDev\FeaturedProducts;
@@ -30,17 +30,15 @@ namespace XLite\Module\CDev\FeaturedProducts;
 /**
  * Featured Products module manager
  *
- * @package XLite
- * @see     ____class_see____
- * @since   1.0.0
+ * @see   ____class_see____
+ * @since 1.0.0
  */
 abstract class Main extends \XLite\Module\AModule
 {
     /**
      * Author name
      *
-     * @var    string
-     * @access public
+     * @return string
      * @see    ____func_see____
      * @since  1.0.0
      */
@@ -49,26 +47,10 @@ abstract class Main extends \XLite\Module\AModule
         return 'Creative Development LLC';
     }
 
-    const FEATURED_PRODUCTS_TABLE = 'xlite_featured_products';
-
-    /**
-     * Module version
-     *
-     * @var    string
-     * @access public
-     * @see    ____func_see____
-     * @since  1.0.0
-     */
-    public static function getMinorVersion()
-    {
-        return '0';
-    }
-
     /**
      * Module name
      *
-     * @var    string
-     * @access public
+     * @return string
      * @see    ____func_see____
      * @since  1.0.0
      */
@@ -78,10 +60,21 @@ abstract class Main extends \XLite\Module\AModule
     }
 
     /**
+     * Module version
+     *
+     * @return string
+     * @see    ____func_see____
+     * @since  1.0.0
+     */
+    public static function getMinorVersion()
+    {
+        return '0';
+    }
+
+    /**
      * Module description
      *
-     * @var    string
-     * @access public
+     * @return string
      * @see    ____func_see____
      * @since  1.0.0
      */
@@ -94,27 +87,11 @@ abstract class Main extends \XLite\Module\AModule
      * Determines if we need to show settings form link
      *
      * @return boolean
-     * @access public
      * @see    ____func_see____
      * @since  1.0.0
      */
     public static function showSettingsForm()
     {
         return true;
-    }
-
-    /**
-     * Get post-installation user notes
-     *
-     * @return string
-     * @access public
-     * @see    ____func_see____
-     * @since  1.0.0
-     */
-    public static function getPostInstallationNotes()
-    {
-        return '<b>Tip:</b> To create featured products, '
-            . 'go to <a href="admin.php?target=categories">Catalog > '
-            . 'Categories</a> menu and add featured product in the bottom form.';
     }
 }
