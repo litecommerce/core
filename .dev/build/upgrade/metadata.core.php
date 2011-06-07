53d212ed40866a46b92a3f1d78c0133228297460
Maxim Shamaev <max@cdev.ru>
2011-06-07 14:40:39 +0400

    [*] Change core packer

--- .dev/build/upgrade/metadata.core.php
+++ .dev/build/upgrade/metadata.core.php
@@ -1,4 +1,29 @@
 <?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * LiteCommerce
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to licensing@litecommerce.com so we can send you a copy immediately.
+ * 
+ * PHP version 5.3.0
+ * 
+ * @category  LiteCommerce
+ * @author    Creative Development LLC <info@cdev.ru> 
+ * @copyright Copyright (c) 2011 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.litecommerce.com/
+ * @see       ____file_see____
+ * @since     1.0.0
+ */
 
 $options = getopt("v:");
 
@@ -8,7 +33,7 @@ if (!isset($options['v'])) {
 
 } elseif (!preg_match('/^(\d+\.\d+)\.(\d+)$/Ss', $options['v'], $match)) {
     echo '-v argument has wrong format' . PHP_EOL;
-    exit (1);
+    exit (2);
 }
 
 $data = array(
