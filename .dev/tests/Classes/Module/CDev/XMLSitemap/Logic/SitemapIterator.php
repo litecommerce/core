ed95731304dc86f00e6fbedb70cf474f7789d7bb
Maxim Shamaev <max@cdev.ru>
2011-11-02 17:45:17 +0400

    S:150575 [*] New test for Module/CDev/XMLSitemap/Logic/SitemapIterator.php is added

--- .dev/tests/Classes/Module/CDev/XMLSitemap/Logic/SitemapIterator.php
+++ .dev/tests/Classes/Module/CDev/XMLSitemap/Logic/SitemapIterator.php
@@ -55,8 +55,8 @@ class XLite_Tests_Module_CDev_XMLSitemap_Logic_SitemapIterator extends XLite_Tes
             $this->assertTrue(time() >= $data['lastmod'], 'check lastmod #' . $i);
             $this->assertEquals('daily', $data['changefreq'], 'check changefreq #' . $i);
 
-            if ('' == $data['loc']['target']) {
-                $this->assertEquals('', $data['loc']['target'], 'check loc target #' . $i);
+            if ('main' == $data['loc']['target']) {
+                $this->assertEquals('main', $data['loc']['target'], 'check loc target #' . $i);
                 $this->assertEquals(0.5, $data['priority'], 'check priority #' . $i);
 
             } elseif ('product' == $data['loc']['target']) {
@@ -79,8 +79,62 @@ class XLite_Tests_Module_CDev_XMLSitemap_Logic_SitemapIterator extends XLite_Tes
             $i,
             'check count'
         );
+    }
 
+    public function testWrongPriority()
+    {
 
+        \XLite\Core\Database::getRepo('XLite\Model\Config')->createOption(
+            array('category' => 'CDev\XMLSitemap', 'name' => 'welcome_priority', 'value' => 'abc')
+        );
+        $iterator = new \XLite\Module\CDev\XMLSitemap\Logic\SitemapIterator;
+        foreach ($iterator as $data) {
+            $this->assertEquals('main', $data['loc']['target'], 'check loc target #1');
+            $this->assertEquals(0.5, $data['priority'], 'check priority #1');
+            break;
+        }        
+
+        \XLite\Core\Database::getRepo('XLite\Model\Config')->createOption(
+            array('category' => 'CDev\XMLSitemap', 'name' => 'welcome_priority', 'value' => 2)
+        );
+        $iterator = new \XLite\Module\CDev\XMLSitemap\Logic\SitemapIterator;
+        foreach ($iterator as $data) {
+            $this->assertEquals('main', $data['loc']['target'], 'check loc target #2');
+            $this->assertEquals(0.5, $data['priority'], 'check priority #2');
+            break;
+        }
+
+        \XLite\Core\Database::getRepo('XLite\Model\Config')->createOption(
+            array('category' => 'CDev\XMLSitemap', 'name' => 'welcome_priority', 'value' => -1)
+        );
+        $iterator = new \XLite\Module\CDev\XMLSitemap\Logic\SitemapIterator;
+        foreach ($iterator as $data) {
+            $this->assertEquals('main', $data['loc']['target'], 'check loc target #3');
+            $this->assertEquals(0.5, $data['priority'], 'check priority #3');
+            break;
+        }
+
+        \XLite\Core\Database::getRepo('XLite\Model\Config')->createOption(
+            array('category' => 'CDev\XMLSitemap', 'name' => 'welcome_priority', 'value' => '0.43')
+        );
+        \XLite\Core\Config::updateInstance();
+        $iterator = new \XLite\Module\CDev\XMLSitemap\Logic\SitemapIterator;
+        foreach ($iterator as $data) {
+            $this->assertEquals('main', $data['loc']['target'], 'check loc target #4');
+            $this->assertEquals(0.4, $data['priority'], 'check priority #4');
+            break;
+        }
+
+        \XLite\Core\Database::getRepo('XLite\Model\Config')->createOption(
+            array('category' => 'CDev\XMLSitemap', 'name' => 'welcome_priority', 'value' => 0.28)
+        );
+        \XLite\Core\Config::updateInstance();
+        $iterator = new \XLite\Module\CDev\XMLSitemap\Logic\SitemapIterator;
+        foreach ($iterator as $data) {
+            $this->assertEquals('main', $data['loc']['target'], 'check loc target #5');
+            $this->assertEquals(0.3, $data['priority'], 'check priority #5');
+            break;
+        }
     }
 }
 
