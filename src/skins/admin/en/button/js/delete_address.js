299d72849138662861092ec131a1d095b076a7e7
Maxim Shamaev <max@cdev.ru>
2011-11-29 22:34:33 +0400

    [*] Added descriptions to files with javascript.

--- src/skins/admin/en/button/js/delete_address.js
+++ src/skins/admin/en/button/js/delete_address.js
@@ -1,7 +1,7 @@
 /* vim: set ts=2 sw=2 sts=2 et: */
 
 /**
- * ____file_title____
+ * Delete address butoon controller
  *
  * @author    Creative Development LLC <info@cdev.ru>
  * @copyright Copyright (c) 2011 Creative Development LLC <info@cdev.ru>. All rights reserved
