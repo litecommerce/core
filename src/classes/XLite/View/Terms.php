c2fe6bb6fec7fd177141c2800da96cf164f0b5f8
Maxim Mukhin <joy@cdev.ru>
2011-12-05 21:08:04 +0400

    E:40655 [+] New: "Terms and conditions" page is added.

---
+++ src/classes/XLite/View/Terms.php
@@ -0,0 +1,95 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * LiteCommerce
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to licensing@litecommerce.com so we can send you a copy immediately.
+ *
+ * PHP version 5.3.0
+ *
+ * @category  LiteCommerce
+ * @author    Creative Development LLC <info@cdev.ru>
+ * @copyright Copyright (c) 2011 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.litecommerce.com/
+ * @see       ____file_see____
+ * @since     1.0.0
+ */
+
+namespace XLite\View;
+
+/**
+ * Cart widget
+ *
+ * @see   ____class_see____
+ * @since 1.0.0
+ *
+ * @ListChild (list="center")
+ */
+class Cart extends \XLite\View\Dialog
+{
+    /**
+     * Return list of targets allowed for this widget
+     *
+     * @return array
+     * @see    ____func_see____
+     * @since  1.0.0
+     */
+    public static function getAllowedTargets()
+    {
+        $result = parent::getAllowedTargets();
+        $result[] = 'terms';
+
+        return $result;
+    }
+
+    /**
+     * Get a list of CSS files required to display the widget properly
+     *
+     * @return array
+     * @see    ____func_see____
+     * @since  1.0.0
+     */
+    public function getCSSFiles()
+    {
+        $list = parent::getCSSFiles();
+
+        return $list;
+    }
+
+    /**
+     * Register JS files
+     *
+     * @return array
+     * @see    ____func_see____
+     * @since  1.0.0
+     */
+    public function getJSFiles()
+    {
+        $list = parent::getJSFiles();
+
+        return $list;
+    }
+
+    /**
+     * Return templates directory name
+     *
+     * @return string
+     * @see    ____func_see____
+     * @since  1.0.0
+     */
+    protected function getDir()
+    {
+        return 'terms_n_conditions';
+    }
+
+}
--- src/skins/default/en/terms_n_conditions/body.tpl
+++ src/skins/default/en/terms_n_conditions/body.tpl
@@ -1,14 +1,13 @@
 {* vim: set ts=2 sw=2 sts=2 et: *}
 
 {**
- * Model
+ * Terms and conditions page
  *
  * @author    Creative Development LLC <info@cdev.ru>
  * @copyright Copyright (c) 2011 Creative Development LLC <info@cdev.ru>. All rights reserved
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link      http://www.litecommerce.com/
- * @since     1.0.13
+ * @since     1.0.0
  *}
-<div class="settings settings-{page}">
-  <widget class="\XLite\View\Model\Settings" useBodyTemplate="1" />
-</div>
+
+{t(#Terms and conditions text#)}
