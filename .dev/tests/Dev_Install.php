d1ba85de437a7d214cc03a7fff55190258498815
Fedotov Daniil <humanoid@humanoid.(none)>
2011-12-13 11:40:18 +0400

    Fixed already rebuilded issue with "cache rebuild" test.

--- .dev/tests/Dev_Install.php
+++ .dev/tests/Dev_Install.php
@@ -9,27 +9,33 @@ require_once "PHPUnit/Extensions/SeleniumTestCase/Driver.php";
 
 class Dev_Install extends PHPUnit_Extensions_SeleniumTestCase{
 
+    static $isCacheRebuilt = false;
     public function setUp()
         {
             $this->setBrowser("*firefox");
-            $this->setBrowserUrl(SELENIUM_SOURCE_URL . "/");
+            if (self::$isCacheRebuilt)
+                $this->setBrowserUrl(SELENIUM_SOURCE_URL . "/");
+            else
+                $this->setBrowserUrl(SELENIUM_SOURCE_URL_ADMIN . "/");
             $this->setHost(SELENIUM_SERVER);
             $this->setPort(4444);
         }
 
         public function testCacheRebuild()
         {
-            $this->setBrowserUrl(SELENIUM_SOURCE_URL_ADMIN . "/");
+
             $this->stop();
             $this->start();
             $this->open('admin.php');
 
-            sleep(30);
-            while (true) {
+            //sleep(30);
+            for($i = 0; $i < 1000; $i++) {
                 if ($this->isTextPresent("Administration Zone"))
                     break;
                 sleep(10);
             }
+            self::$isCacheRebuilt = true;
+
         }
 
         public function testInstall()
