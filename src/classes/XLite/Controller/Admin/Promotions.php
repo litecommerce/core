2d09dc51c1b7b085739c702a473ef7fb499864c6
Maxim Shamaev <max@cdev.ru>
2012-02-08 10:09:48 +0400

    S:161756 [*] Add Promotions dump page into Admin interface

---
+++ src/classes/XLite/Controller/Admin/Promotions.php
@@ -0,0 +1,94 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * LiteCommerce
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to licensing@litecommerce.com so we can send you a copy immediately.
+ * 
+ * PHP version 5.3.0
+ * 
+ * @category  LiteCommerce
+ * @author    Creative Development LLC <info@cdev.ru> 
+ * @copyright Copyright (c) 2011 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.litecommerce.com/
+ * @see       ____file_see____
+ * @since     1.0.17
+ */
+
+namespace XLite\Controller\Admin;
+
+/**
+ * Common promotions controller
+ * 
+ * @see   ____class_see____
+ * @since 1.0.17
+ */
+class Promotions extends \XLite\Controller\Admin\AAdmin
+{
+    /**
+     * FIXME- backward compatibility
+     *
+     * @var   array
+     * @see   ____var_see____
+     * @since 1.0.0
+     */
+    protected $params = array('target', 'page');
+
+    /**
+     * Get pages static 
+     * 
+     * @return array
+     * @see    ____func_see____
+     * @since  1.0.17
+     */
+    public static function getPagesStatic()
+    {
+        return array();
+    }
+
+    /**
+     * Get pages sections
+     *
+     * @return array
+     * @see    ____func_see____
+     * @since  1.0.0
+     */
+    public function getPages()
+    {
+        $list = array();
+
+        foreach (static::getPagesStatic() as $key => $page) {
+            $list[$key] = $page['name'];
+        }
+
+        return $list;
+    }
+
+    /**
+     * Get pages templates
+     *
+     * @return array
+     * @see    ____func_see____
+     * @since  1.0.0
+     */
+    public function getPageTemplates()
+    {
+        $list = array();
+
+        foreach (static::getPagesStatic() as $key => $page) {
+            $list[$key] = $page['tpl'];
+        }
+
+        return $list;
+    }
+}
