647d4f934ea5088ea02c52140149538f7631e583
Vadim Sannikov <vvs@cdev.ru>
2012-02-15 07:46:58 +0400

    [+] "Version" page is added

---
+++ src/classes/XLite/Controller/Customer/Version.php
@@ -0,0 +1,98 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * LiteCommerce
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to licensing@litecommerce.com so we can send you a copy immediately.
+ * 
+ * PHP version 5.3.0
+ * 
+ * @category  LiteCommerce
+ * @author    Creative Development LLC <info@cdev.ru> 
+ * @copyright Copyright (c) 2011 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.litecommerce.com/
+ * @see       ____file_see____
+ * @since     1.0.17
+ */
+
+namespace XLite\Controller\Customer;
+
+/**
+ * Version 
+ *
+ * @see   ____class_see____
+ * @since 1.0.17
+ */
+class Version extends \XLite\Controller\Customer\ACustomer
+{
+    /**
+     * Preprocessor for no-action ren
+     *
+     * @return void
+     * @see    ____func_see____
+     * @since  1.0.0
+     */
+    protected function doNoAction()
+    {
+        \Includes\Utils\Operator::flush($this->getInfoMessage());
+
+        exit(0);
+    }
+
+    /**
+     * Method to compose different messages
+     *
+     * @return string
+     * @see    ____func_see____
+     * @since  1.0.17
+     */
+    protected function getInfoMessage()
+    {
+        $result = '';
+
+        $result .= $this->getVersionMessage() . LC_EOL;
+        $result .= $this->getModulesMessage() . LC_EOL;
+
+        return $result;
+    }
+
+    /**
+     * Return info about current LC version
+     *
+     * @return string
+     * @see    ____func_see____
+     * @since  1.0.17
+     */
+    protected function getVersionMessage()
+    {
+        return static::t('Version') . ': ' . \XLite::getInstance()->getVersion() . LC_EOL;
+    }
+
+    /**
+     * Return info about installed modules
+     *
+     * @return string
+     * @see    ____func_see____
+     * @since  1.0.17
+     */
+    protected function getModulesMessage()
+    {
+        $result = array();
+
+        foreach (\Includes\Utils\ModulesManager::getActiveModules() as $data) {
+            $result[] = '(' . $data['authorName'] . '): ' . $data['moduleName'];
+        }
+
+        return 'Installed modules:' . LC_EOL . ($result ? implode(LC_EOL, $result) : static::t('None'));
+    }
+}
