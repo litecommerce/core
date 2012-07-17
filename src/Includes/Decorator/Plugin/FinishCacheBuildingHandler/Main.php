1fff069eb1c3d57fed5fcec5c754d0cfa65ef0f9
Maxim Mukhin <joy@cdev.ru>
2012-07-17 16:29:45 +0400

    [!] Wrong is_enabled element definition.

--- src/Includes/Decorator/Plugin/FinishCacheBuildingHandler/Main.php
+++ src/Includes/Decorator/Plugin/FinishCacheBuildingHandler/Main.php
@@ -44,6 +44,8 @@ class Main extends \Includes\Decorator\Plugin\APlugin
      */
     public function executeHookHandler()
     {
+        \Includes\Utils\FileManager::unlinkRecursive(LC_DIR_CACHE_RESOURCES);
+        
         \XLite\Core\Database::getRepo('XLite\Model\TmpVar')->setVar(\XLite::CACHE_TIMESTAMP, intval(microtime(true)));
     }
 }
