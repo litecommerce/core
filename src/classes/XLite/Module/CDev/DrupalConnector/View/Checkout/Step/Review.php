18bf923638014257299054cb5daf16f18ea3e070
Vladimir Semenov <svowl@cdev.ru>
2012-07-24 19:30:11 +0400

    [*] Headers updated for GPL files

--- src/classes/XLite/Module/CDev/DrupalConnector/View/Checkout/Step/Review.php
+++ src/classes/XLite/Module/CDev/DrupalConnector/View/Checkout/Step/Review.php
@@ -18,11 +18,9 @@
  *
  * @category  LiteCommerce
  * @author    Creative Development LLC <info@cdev.ru>
- * @copyright Copyright (c) 2011 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @copyright Copyright (c) 2011-2012 Creative Development LLC <info@cdev.ru>. All rights reserved
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU General Pubic License (GPL 2.0)
  * @link      http://www.litecommerce.com/
- * @see       ____file_see____
- * @since     1.0.0
  */
 
 namespace XLite\Module\CDev\DrupalConnector\View\Checkout\Step;
