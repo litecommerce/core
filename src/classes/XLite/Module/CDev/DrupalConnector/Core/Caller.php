55779da805205b25b27001ec57ddfb5839d32e43
Maxim Shamaev <max@cdev.ru>
2012-08-03 04:08:00 +0400

    E:41232 [*] Calculating the URL of the admin interface guidelines require adjustment 'base_url' in the configuration file Drupal. The algorithm for calculation has been changed to this setup is no longer needed.

--- src/classes/XLite/Module/CDev/DrupalConnector/Core/Caller.php
+++ src/classes/XLite/Module/CDev/DrupalConnector/Core/Caller.php
@@ -38,6 +38,8 @@ class Caller extends \XLite\base\Singleton
      */
     protected $initialized;
 
+    protected $oldScriptName;
+
     // {{{ Initialization
 
     /**
@@ -52,19 +54,98 @@ class Caller extends \XLite\base\Singleton
             && \XLite\Core\Config::getInstance()->CDev->DrupalConnector->drupal_root_path
             && !isset($this->initialized)
         ) {
+            $this->initialized = false;
+
             if (file_exists(\XLite\Core\Config::getInstance()->CDev->DrupalConnector->drupal_root_path . '/includes/bootstrap.inc')) {
 
                 define('DRUPAL_ROOT', \XLite\Core\Config::getInstance()->CDev->DrupalConnector->drupal_root_path);
                 require_once (DRUPAL_ROOT . '/includes/bootstrap.inc');
-                drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-                $this->initialized = true;
-
-            } else {
-                $this->initialized = false;
+                if ($this->initializeDrupalStart()) {
+                    drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+                    $this->finalizeDrupalStart();
+                    $this->initialized = true;
+                }
             }
 
         } elseif (defined('DRUPAL_ROOT')) {
             $this->initialized = true;
+        }
+    }
+
+    /**
+     * Initialize Drupal start routine
+     * 
+     * @return boolean
+     */
+    protected function initializeDrupalStart()
+    {
+        $result = true;
+
+        $this->oldScriptName = null;
+
+        if (!empty($_SERVER['SCRIPT_NAME'])) {
+            $this->oldScriptName = $_SERVER['SCRIPT_NAME'];
+
+            $currentPath = getcwd();
+            $drupalPath = \XLite\Core\Config::getInstance()->CDev->DrupalConnector->drupal_root_path;
+
+            // Search common path
+            $dirIndex = 0;
+            for ($i = 0; $i < strlen($currentPath); $i++) {
+                if (substr($currentPath, $i, 1) == substr($drupalPath, $i, 1)) {
+                    if (DIRECTORY_SEPARATOR == substr($drupalPath, $i, 1)) {
+                        $dirIndex = $i;
+                    }
+
+                } else {
+                    break;
+                }
+            }
+
+            if (0 == $dirIndex) {
+
+                // No common path
+                $drupalScriptName = str_replace(DIRECTORY_SEPARATOR, '/', $drupalPath) . '/index.php';
+
+            } else {
+
+                // Crop common path from file-system paths
+                $currentPathDiff = substr($currentPath, $dirIndex);
+                $drupalPathDiff = substr($drupalPath, $dirIndex);
+
+                // Search common path into web-path
+                $scriptNameFS = str_replace('/', DIRECTORY_SEPARATOR, $_SERVER['SCRIPT_NAME']);
+                $unionPos = strpos($scriptNameFS, $currentPathDiff);
+
+                if (false === $unionPos) {
+
+                    // Web path and file-system path not equal
+                    $result = false;
+
+                } else {
+                    $drupalScriptName = substr($_SERVER['SCRIPT_NAME'], 0, $unionPos)
+                        . str_replace(DIRECTORY_SEPARATOR, '/', $drupalPathDiff)
+                        . '/index.php';
+                }
+            }
+
+            if ($result) {
+                $_SERVER['SCRIPT_NAME'] = $drupalScriptName;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Finalize Drupal start routine
+     *
+     * @return void
+     */
+    protected function finalizeDrupalStart()
+    {
+        if ($this->oldScriptName) {
+            $_SERVER['SCRIPT_NAME'] = $this->oldScriptName;
         }
     }
 
