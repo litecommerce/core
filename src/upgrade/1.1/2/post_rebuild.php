4a696b84304be1aed1501403b33f775046c103bc
Maxim Shamaev <max@cdev.ru>
2012-10-21 10:57:36 +0400

    E:42318 [!] Bug: upgrade script did set payment method type. Fixed.

--- src/upgrade/1.1/2/post_rebuild.php
+++ src/upgrade/1.1/2/post_rebuild.php
@@ -25,6 +25,68 @@
 
 return function()
 {
+
+    $types = array(
+        'PurchaseOrder'      => 'O',
+        'PhoneOrdering'      => 'O',
+        'FaxOrdering'        => 'O',
+        'MoneyOrdering'      => 'O',
+        'Echeck'             => 'O',
+        'COD'                => 'O',
+        'AuthorizeNet SIM'   => 'C',
+        'Moneybookers.WLT'   => 'N',
+        'Moneybookers.ACC'   => 'C',
+        'Moneybookers.VSA'   => 'C',
+        'Moneybookers.MSC'   => 'C',
+        'Moneybookers.VSD'   => 'C',
+        'Moneybookers.VSE'   => 'C',
+        'Moneybookers.MAE'   => 'C',
+        'Moneybookers.SLO'   => 'C',
+        'Moneybookers.AMX'   => 'C',
+        'Moneybookers.DIN'   => 'C',
+        'Moneybookers.JCB'   => 'C',
+        'Moneybookers.LSR'   => 'C',
+        'Moneybookers.GCB'   => 'C',
+        'Moneybookers.DNK'   => 'C',
+        'Moneybookers.PSP'   => 'C',
+        'Moneybookers.CSI'   => 'C',
+        'Moneybookers.OBT'   => 'N',
+        'Moneybookers.GIR'   => 'N',
+        'Moneybookers.DID'   => 'N',
+        'Moneybookers.SFT'   => 'N',
+        'Moneybookers.ENT'   => 'N',
+        'Moneybookers.EBT'   => 'N',
+        'Moneybookers.SO'    => 'N',
+        'Moneybookers.IDL'   => 'N',
+        'Moneybookers.NPY'   => 'N',
+        'Moneybookers.PLI'   => 'N',
+        'Moneybookers.PWY'   => 'N',
+        'Moneybookers.PWY5'  => 'N',
+        'Moneybookers.PWY6'  => 'N',
+        'Moneybookers.PWY7'  => 'N',
+        'Moneybookers.PWY14' => 'N',
+        'Moneybookers.PWY15' => 'N',
+        'Moneybookers.PWY17' => 'N',
+        'Moneybookers.PWY18' => 'N',
+        'Moneybookers.PWY19' => 'N',
+        'Moneybookers.PWY20' => 'N',
+        'Moneybookers.PWY21' => 'N',
+        'Moneybookers.PWY22' => 'N',
+        'Moneybookers.PWY25' => 'N',
+        'Moneybookers.PWY26' => 'C',
+        'Moneybookers.PWY28' => 'N',
+        'Moneybookers.PWY32' => 'N',
+        'Moneybookers.PWY33' => 'N',
+        'Moneybookers.PWY36' => 'N',
+        'Moneybookers.EPY'   => 'N',
+        'PayflowLink'        => 'C',
+        'PaypalAdvanced'     => 'A',
+        'PaypalWPSUS'        => 'A',
+        'ExpressCheckout'    => 'N',
+        'QuantumGateway'     => 'C',
+        '2Checkout.com'      => 'C',
+    );
+
     // Get disabled modules
     $classes = array();
     $cnd = new \XLite\Core\CommonCell;
@@ -38,8 +100,13 @@ return function()
         $parts = explode('\\', $method->getClass());
         $class = implode('\\', array_slice($parts, 1, 2));
         $method->setModuleEnabled(!in_array($class, $classes));
+
         if (!$method->getAdded() && $method->getType() == \XLite\Model\Payment\Method::TYPE_OFFLINE) {
             $method->setAdded(true);
+        }
+
+        if (!$method->getType()) {
+            $method->setType(isset($types[$method->getServiceName()]) ? $types[$method->getServiceName()] : 'C');
         }
     }
     \XLite\Core\Database::getEM()->flush();
