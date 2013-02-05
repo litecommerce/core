357535f7587b2bd48dc9f089f5ad5aba65ed3a00
Sergei Fundaev <fundaevs@gmail.com>
2013-02-05 20:35:28 +0400

    [*] Small corrections in the files headers

--- src/classes/XLite/Module/AModule.php
+++ src/classes/XLite/Module/AModule.php
@@ -1,5 +1,4 @@
 <?php
-
 // vim: set ts=4 sw=4 sts=4 et:
 
 /**
--- src/classes/XLite/Module/CDev/ProductOptions/View/Order/Details/Admin/Info.php
+++ src/classes/XLite/Module/CDev/ProductOptions/View/Order/Details/Admin/Info.php
@@ -3,9 +3,9 @@
 
 /**
  * LiteCommerce
- * 
+ *
  * NOTICE OF LICENSE
- * 
+ *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
