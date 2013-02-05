2c13c06667df2c7de01469d2388899cc56a7b841
Sergei Fundaev <fundaevs@gmail.com>
2013-02-05 20:42:34 +0400

    [*] Small corrections in the files headers - 2.

--- src/classes/XLite/Module/CDev/ProductOptions/View/Order/Details/Admin/Info.php
+++ src/classes/XLite/Module/CDev/ProductOptions/View/Order/Details/Admin/Info.php
@@ -17,7 +17,7 @@
  * PHP version 5.3.0
  * 
  * @category  LiteCommerce
- * @author    Creative Development LLC <info@cdev.ru> 
+ * @author    Creative Development LLC <info@cdev.ru>
  * @copyright Copyright (c) 2010-2012 Creative Development LLC <info@cdev.ru>. All rights reserved
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link      http://www.litecommerce.com/
